901ce82beace72c2b318a62f0bb6f09de4da54f4
DK96 <69859316+DK96-OS@users.noreply.github.com>
2020-08-24 21:19:14 -0400

    Create PrimeCounter.kt

---
+++ Numbers/Test/PrimeCounter.kt
@@ -0,0 +1,10 @@
+/** Counter for Primes
+  * Developed by DK96-OS 2018 - 2020 */
+class PrimeCounter(
+    val product: Long
+)
+    : Counter() {
+
+    override fun counterToString() = "$product : $count"
+
+}
