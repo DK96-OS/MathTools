f1dac732e24bf86701a97bd075d5687ed9160d22
DK96 <69859316+DK96-OS@users.noreply.github.com>
2020-08-24 21:10:16 -0400

    Create Counter.kt

---
+++ Numbers/Test/Counter.kt
@@ -0,0 +1,10 @@
+/** Counts the number of some occurrence 
+  * Developed by DK96-OS 2018 - 2020 */
+abstract class Counter(
+    var count: Int = 1
+) {
+
+    /** Must provide a string method */
+    abstract fun counterToString(): String
+
+}
