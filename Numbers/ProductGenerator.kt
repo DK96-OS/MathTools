66c8940dcb8e090c6ec1552125b4cb7d4ec5c51c
DK96 <69859316+DK96-OS@users.noreply.github.com>
2020-08-26 15:17:55 -0400

    Removed function from ProductGenerator.kt
    The function was moved to PrimeNumberTools because it is a static function and it is concerned with Prime Numbers.

--- Numbers/ProductGenerator.kt
+++ Numbers/ProductGenerator.kt
@@ -34,37 +34,13 @@ class ProductGenerator(
     override fun generate() {
         product = range.random(rng)
         if (maxPrime < product) {
-            var primeAboveLimit = getFirstPrimeAboveLimit(product, maxPrime)
+            var primeAboveLimit = PrimeNumberTools.getFirstPrimeAboveLimit(product, maxPrime)
             while (primeAboveLimit != null) {
                 product /= primeAboveLimit
                 product *= (primeAboveLimit - if (Math.random() > 0.5) 1 else -1)
-                primeAboveLimit = getFirstPrimeAboveLimit(product, maxPrime)
+                primeAboveLimit = PrimeNumberTools.getFirstPrimeAboveLimit(product, maxPrime)
             }
         }
-    }
-
-    /** Checks for a Prime Number Factor in product that is greater than the limit
-     * @param product The product to search in
-     * @param limit The maximum prime factor that is allowed
-     * @return The smallest prime factor above the limit, or null  */
-    private fun getFirstPrimeAboveLimit(product: Long, limit: Long): Long? {
-        var remainingFactors = product
-        while (remainingFactors % 2 == 0L) remainingFactors /= 2  // Discard all 2 values
-        if (limit < remainingFactors) { // Is there a potential factor greater than the remainder
-            var primeCounter = 3L  // Need to remove all primes below or equal to the limit
-            while (limit in primeCounter until remainingFactors) {
-                if (remainingFactors % primeCounter == 0L)
-                    remainingFactors /= primeCounter    // Throw away
-                else primeCounter += 2   // Increment
-            }
-            if (limit < remainingFactors) {
-                while (primeCounter <= remainingFactors) {
-                    if (remainingFactors % primeCounter == 0L) return primeCounter
-                    else primeCounter++
-                }
-            }
-        }
-        return null
     }
 
 }
