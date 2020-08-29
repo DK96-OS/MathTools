acf83cc5fdb4029eaed2537f6c56e03ba321fdb0
DK96 <69859316+DK96-OS@users.noreply.github.com>
2020-08-28 20:43:26 -0400

    Update GeneratorAnalysis.kt
    Added Standard Deviation, and Standard Error functions. Updated print functions with latest parameters.

--- Numbers/Test/GeneratorAnalysis.kt
+++ Numbers/Test/GeneratorAnalysis.kt
@@ -46,7 +46,7 @@ abstract class GeneratorAnalysis<G: Generator, C: Counter>(
 
     /** Determines the Median value of all counters
      * @param sortedAscending Pass true if counters have already been sorted, ascending. */
-    fun calculateMedian(sortedAscending: Boolean = false): Float {
+    fun getMedian(sortedAscending: Boolean = false): Float {
         if (counterList.size < 3) throw IllegalStateException()
         if (!sortedAscending) sortCounters(true)
         val halfIndex = counterList.size / 2
@@ -55,6 +55,27 @@ abstract class GeneratorAnalysis<G: Generator, C: Counter>(
         else
             (counterList[halfIndex].count + counterList[halfIndex + 1].count) / 2f
     }
+
+    /** Calculates the Standard Deviation of the Counters from the Mean */
+    fun getStandardDeviation(): Float {
+        if (counterList.size < 2) throw IllegalStateException()
+        val mean = meanCount.toDouble()
+        var varianceSum = 0.0
+        counterList.forEach {
+            val deviation = it.count - mean
+            varianceSum += deviation * deviation
+        }
+        return sqrt(varianceSum / (counterList.size - 1)).toFloat()
+    }
+
+    /** Calculates the Standard Error, the Standard Deviation of the Mean 
+     * @param sDev Provide the Standard Deviation if already known */
+    fun getStandardError(sDev: Float? = null)
+        : Float = ((sDev ?: getStandardDeviation()) / sqrt(totalCycles.toDouble())).toFloat()
+
+    /** Calculates the Fractional Error in the Error
+     * Helps determine the appropriate number of measurements to minimize the error */
+    fun getErrorInError(): Float = (1.0 / sqrt(2.0 * totalCycles - 2.0)).toFloat()
 
     /** Call between each Test */
     fun clearCounters() {
@@ -70,13 +91,17 @@ abstract class GeneratorAnalysis<G: Generator, C: Counter>(
         }
     }
 
-    /** Prints the Mean counter value and it's probability */
-    fun printMeanValues() { println("Mean: ($meanCount, $meanPercent %)") }
+    /** Prints the Mean value, Standard Deviation and Error */
+    fun printMeanValues() {
+        val sDev = getStandardDeviation()
+        val sError = getStandardError(sDev) * (1f + getErrorInError())
+        println("Mean: ($meanCount, $meanPercent %) +/- $sError \nStandard Deviation: $sDev")
+    }
 
     /** Determines the Median counter value, as well as the differences
      * @param sortedAscending Pass true if counters have already been sorted, ascending. */
     fun printMedianRange(sortedAscending: Boolean = false) {
-        val median = calculateMedian(sortedAscending)
+        val median = getMedian(sortedAscending)
         val total = totalCycles / 100f
         println("Median: ($median, ${median / total} %)")
         val min = counterList[0].count
