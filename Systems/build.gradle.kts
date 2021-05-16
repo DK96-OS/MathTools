d9431f3edda79570fc798ba31c8910c812687f88
DK96 <69859316+DK96-OS@users.noreply.github.com>
2021-05-16 19:32:04 -0400

    Reorganized System module into Gradle project

---
+++ Systems/build.gradle.kts
@@ -0,0 +1,23 @@
+
+plugins {
+    // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
+    id("org.jetbrains.kotlin.jvm") version "1.4.32"
+}
+
+repositories {
+    maven()
+}
+
+dependencies {
+    // Align versions of all Kotlin components
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+
+    // Use the Kotlin JDK 8 standard library.
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // Use the Kotlin test library.
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+
+    // Use the Kotlin JUnit integration.
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
