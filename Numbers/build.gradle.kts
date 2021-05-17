9021374a5552b437ad89c9549c578ffc78148681
DK96 <69859316+DK96-OS@users.noreply.github.com>
2021-05-16 23:37:17 -0400

    Gradle build file setup complete

--- Numbers/build.gradle.kts
+++ Numbers/build.gradle.kts
@@ -1,11 +1,5 @@
-
 plugins {
-    // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.4.32"
-}
-
-repositories {
-    maven()
+	kotlin("jvm")
 }
 
 dependencies {
