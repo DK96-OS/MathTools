ed6258da428283ada55b4125bbd6ebcf128e7b9b
DK96 <69859316+DK96-OS@users.noreply.github.com>
2021-05-17 16:28:20 -0400

    Prime Number Tools update\n* Renamed redFactor to reduceByFactor\n* Code style updates

--- Numbers/src/main/kotlin/data/PrimeNumberTools.kt
+++ Numbers/src/main/kotlin/data/PrimeNumberTools.kt
@@ -1,3 +1,4 @@
+package data
 
 /** Container for PrimeNumber functions */
 object PrimeNumberTools {
@@ -30,36 +31,43 @@ object PrimeNumberTools {
         }
     }
 
-    /** Determines whether this Product/Prime contains a prime number greater than the limit
+    /** Whether this Product/Prime contains a prime number greater than the limit
      * @param product The product to test
      * @param limit The maximum prime number allowed */
     fun checkForPrimeFactorAboveLimit(product: Long, limit: Long)
-        : Boolean = if (limit < product) {
+    : Boolean = if (limit < product) {
         var composite = product
-        while (composite % 2 == 0L && composite > limit) composite /= 2
-        if (limit >= 3) while (composite % 3 == 0L && composite > limit) composite /= 3
-        if (limit >= 5) while (composite % 5 == 0L && composite > limit) composite /= 5
+        while (composite % 2 == 0L && composite > limit) 
+        	composite /= 2
+        if (limit >= 3) 
+        	while (composite % 3 == 0L && composite > limit) 
+        		composite /= 3
+        if (limit >= 5) 
+        	while (composite % 5 == 0L && composite > limit) 
+        		composite /= 5
         if (composite <= limit) false else {
             var primeCounter = 7L
             while (limit in primeCounter until composite) {
-                while (composite % primeCounter == 0L) composite /= primeCounter
+                while (composite % primeCounter == 0L) 
+                	composite /= primeCounter
                 primeCounter += 2
-                if (primeCounter.toString().endsWith('5')) primeCounter += 2
+                if (primeCounter.toString().endsWith('5')) 
+                	primeCounter += 2
             }
             composite > limit
         }
     } else false
 
-    /** Obtains the lowest Prime Number Factor that is greater than the limit or null
+    /** Obtain lowest Prime Number Factor that is greater than the limit
      * @param product The product to search in
      * @param limit The maximum prime factor that is allowed
      * @return The smallest prime factor above the limit, or null  */
     fun getFirstPrimeAboveLimit(product: Long, limit: Long): Long? {
-        var composite = if (limit >= 2) redFactor(2, product)
+        var composite = if (limit >= 2) reduceByFactor(2, product)
         else throw IllegalArgumentException()
         var primeCounter = 3
         while (limit in primeCounter until composite) { // Safe to remove
-            val reduced = redFactor(primeCounter, composite)
+            val reduced = reduceByFactor(primeCounter, composite)
             if (reduced < composite) composite = reduced
             primeCounter += 2
         }
@@ -71,12 +79,13 @@ object PrimeNumberTools {
     }
 
     /** Reduces the composite by the given factor repeatedly
-     * @param f The factor to reduce
-     * @param composite The composite number to be reduced if it contains f
+     * @param factor The factor to reduce
+     * @param composite The composite number to be reduced if it contains factor
      * @return The reduced or unreduced composite number */
-    fun redFactor(f: Int, composite: Long): Long = if (composite % f == 0L) {
-        var reduced = composite / f
-        while (reduced % f == 0L) { reduced /= f }
+    fun reduceByFactor(factor: Int, composite: Long)
+    : Long = if (composite % factor == 0L) {
+        var reduced = composite / factor
+        while (reduced % factor == 0L) { reduced /= factor }
         reduced
     } else composite
     
