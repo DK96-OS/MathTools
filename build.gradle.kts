b76a347639fc2043d0d1e1d5768e7c6cba334658
DK96 <69859316+DK96-OS@users.noreply.github.com>
2021-05-17 09:28:23 -0400

    Initial Actions Workflow (#7)
    Create gradle.yml for running compilation and unit tests.
    * Create Gradle root project build files
    * Reorganize Numbers module into a Gradle project
    * Reorganize System module into Gradle project
    
    Bug Fix: 
    * Missing import fixed
    * Float assertion accuracy relaxed in statistics tests (variable outcomes)
    
    Changed License to GPL 3

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+buildscript {
+	repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+    	classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.31")
+   	}
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+/*
+ * The settings file is used to specify which projects to include in your build
+ * Detailed information about configuring a multi-project build in Gradle can be found
+ * in the user manual at https://docs.gradle.org/6.8.3/userguide/multi_project_builds.html
+ */
+ 
+pluginManagement {
+	plugins {
+		kotlin("jvm")
+	}
+	repositories {
+		google()
+	    maven(url = "https://plugins.gradle.org/m2/")
+	    mavenCentral()
+	    gradlePluginPortal()
+	}
+}
+rootProject.name = "MathTools"
+include("Numbers")
+include("Systems")
+
