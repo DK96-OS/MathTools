ab72788b8f520f5e55bfe34e85d6f06dafd13fd7
DK96 <69859316+DK96-OS@users.noreply.github.com>
2021-05-20 20:13:35 -0400

    Update build.gradle.kts
    * Add Coroutine support
    * Add JUnit 5 Jupiter support

--- Systems/build.gradle.kts
+++ Systems/build.gradle.kts
@@ -9,9 +9,24 @@ dependencies {
     // Use the Kotlin JDK 8 standard library.
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
+    // Coroutine support
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
+    
     // Use the Kotlin test library.
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 
     // Use the Kotlin JUnit integration.
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+    
+    // Use JUnit 5
+    testImplementation(platform("org.junit:junit-bom:5.7.2"))
+    testImplementation('org.junit.jupiter:junit-jupiter-api')
+    
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "failed") // not including "skipped"
+    }
 }
