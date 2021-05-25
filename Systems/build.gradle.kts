a857f9fce18968b4b88c30231aac511991712a25
DK96 <69859316+DK96-OS@users.noreply.github.com>
2021-05-25 10:28:49 -0400

    Fix Systems Build file

--- Systems/build.gradle.kts
+++ Systems/build.gradle.kts
@@ -3,30 +3,31 @@ plugins {
 }
 
 dependencies {
-    // Align versions of all Kotlin components
+     // Align versions of all Kotlin components
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-
-    // Use the Kotlin JDK 8 standard library.
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    // Coroutine support
+	// Coroutine support
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.0")
     
-    // Use the Kotlin test library.
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-
-    // Use the Kotlin JUnit integration.
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
-    
-    // Use JUnit 5
+    // JUnit 5
     testImplementation(platform("org.junit:junit-bom:5.7.2"))
-    testImplementation('org.junit.jupiter:junit-jupiter-api')
-    
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testImplementation("org.junit.jupiter:junit-jupiter-params")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
 tasks.test {
     useJUnitPlatform()
-    testLogging {
+	maxParallelForks = 4
+	testLogging {
         events("passed", "failed") // not including "skipped"
+    }
+    reports {
+    	junitXml.apply {
+    		isOutputPerTestCase = true
+    		mergeReruns.set(true)
+    	}
     }
 }
