fe335351a71933cddae6379d4daad0104f95b1b3
DK96 <69859316+DK96-OS@users.noreply.github.com>
2021-07-05 15:25:50 -0400

    Update PrimeCachePerformanceTest: * Add Statistics import for easy data summary * Create FindPrime method test case, which collects nanosecond time measurements for various prime number search cases

--- Numbers/src/test/kotlin/mathtools/numbers/primes/PrimeCachePerformanceTest.kt
+++ Numbers/src/test/kotlin/mathtools/numbers/primes/PrimeCachePerformanceTest.kt
@@ -5,11 +5,32 @@ import org.junit.jupiter.api.RepeatedTest
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
+import mathtools.numbers.statistics.Statistics
+import kotlin.system.measureNanoTime
 
 /** Performance Testing for Prime Number Caches */
 class PrimeCachePerformanceTest {
 
     private val primes: List<Int> = BytePrimeCacheTest.primeList
+
+    @ParameterizedTest
+    @ValueSource(ints = [9, 9, 9, 15, 15, 21, 25, 27, 29, 31, 33, 35,
+    55, 55, 55, 71, 71, 71, 75, 75, 75, 77, 77, 77])
+    fun testFindPrime(startNumber: Int) {
+        val cache = BytePrimeCache()
+        val timeMeasurements = Array<Long>(5_000) {0}
+        for (i in timeMeasurements.indices) {
+            timeMeasurements[i] = measureNanoTime {
+                cache.findPrime(startNumber)
+            }
+            cache.clear()
+        }
+        val dataList = timeMeasurements.toList()
+        val mean = Statistics.calculateMeanLong(dataList)
+        val sDev = Statistics.calculateStandardDeviation(dataList)
+        println("FindPrimeResult ($startNumber) : u=$mean, o-=$sDev")
+    }
+
 
 	@ParameterizedTest
 	@ValueSource(ints = [0, 4, 9, 12, 15, 16, 17, 18, 20,
