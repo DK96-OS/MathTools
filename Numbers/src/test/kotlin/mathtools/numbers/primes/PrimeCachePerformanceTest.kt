9e18f479daf061eac804e57254ffad57b91f0eb0
DK96 <69859316+DK96-OS@users.noreply.github.com>
2021-07-05 23:46:34 -0400

    Update PrimeCachePerformanceTest: * Fix PrintTestResults, now removing the 3 longest measurements * Wrapped TargetedAccess Test in coroutines

--- Numbers/src/test/kotlin/mathtools/numbers/primes/PrimeCachePerformanceTest.kt
+++ Numbers/src/test/kotlin/mathtools/numbers/primes/PrimeCachePerformanceTest.kt
@@ -40,27 +40,31 @@ class PrimeCachePerformanceTest {
 	@ValueSource(ints = [0, 4, 9, 12, 15, 16, 17, 18, 20,
 	22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 53])
     fun testTargetedAccess(target: Int) {
-        val cache = BytePrimeCache()
-        val prime = primes[target]
-        val timeMeasurements = ArrayList<Long>(10_000)
-        var foundPrime: Int? = null
-        for (i in 0 until 10_000) {
-            timeMeasurements.add(measureNanoTime {
-                foundPrime = cache.getPrime(target)
-            })
-            assertEquals(prime, foundPrime)
-            cache.clear()
-        }
-        printTestResults(
-            "TestTargetedAccess", "$target, $foundPrime", timeMeasurements)
+        runBlocking { coroutineScope { launch(Dispatchers.IO) {
+            val cache = BytePrimeCache()
+            val prime = primes[target]
+            val timeMeasurements = ArrayList<Long>(10_000)
+            var foundPrime: Int? = null
+            for (i in 0 until 10_000) {
+                timeMeasurements.add(measureNanoTime {
+                    foundPrime = cache.getPrime(target)
+                })
+                assertEquals(prime, foundPrime)
+                cache.clear()
+            }
+            printTestResults(
+                "TestTargetedAccess", "$target, $foundPrime", timeMeasurements)
+        } } }
     }
 
     /** Process and print a statistical summary of the data */
     internal fun printTestResults(
         testName: String, params: String, dataList: ArrayList<Long>
     ) {
-        val max = timeMeasurements.maxOf {it}
-        timeMeasurements.removeIf {it == max}
+        repeat (3) {    // Remove the top 3 longest measurements
+            val max = dataList.maxOrNull()
+            dataList.removeIf {it == max}
+        }
         val mean = Statistics.calculateMeanLong(dataList)
         val sDev = Statistics.calculateStandardDeviation(dataList)
         val min = dataList.minOrNull()
