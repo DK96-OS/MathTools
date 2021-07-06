d05600a9f405f4e7650b11c99cc84b1a8ade2d03
DK96 <69859316+DK96-OS@users.noreply.github.com>
2021-07-05 23:33:46 -0400

    Update PrimeCachePerformanceTest: * Create Print function for processing and displaying time results * Improve FindPrime Test stability by wrapping in a coroutine * Refactor TargetedAccess Test to use new measurement process

--- Numbers/src/test/kotlin/mathtools/numbers/primes/PrimeCachePerformanceTest.kt
+++ Numbers/src/test/kotlin/mathtools/numbers/primes/PrimeCachePerformanceTest.kt
@@ -7,6 +7,10 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 import mathtools.numbers.statistics.Statistics
 import kotlin.system.measureNanoTime
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 /** Performance Testing for Prime Number Caches */
 class PrimeCachePerformanceTest {
@@ -14,23 +18,23 @@ class PrimeCachePerformanceTest {
     private val primes: List<Int> = BytePrimeCacheTest.primeList
 
     @ParameterizedTest
-    @ValueSource(ints = [9, 9, 9, 15, 15, 21, 25, 27, 29, 31, 33, 35,
+    @ValueSource(ints = [9, 9, 15, 15, 21, 25, 25, 27, 29, 33, 35,
     55, 55, 55, 71, 71, 71, 75, 75, 75, 77, 77, 77])
     fun testFindPrime(startNumber: Int) {
-        val cache = BytePrimeCache()
-        val timeMeasurements = Array<Long>(5_000) {0}
-        for (i in timeMeasurements.indices) {
-            timeMeasurements[i] = measureNanoTime {
-                cache.findPrime(startNumber)
+        runBlocking { coroutineScope { launch(Dispatchers.IO) {
+            val cache = BytePrimeCache()
+            val timeMeasurements = ArrayList<Long>(10_000)
+            var foundPrime: Int? = null
+            for (i in 0 until 10_000) {
+                timeMeasurements.add(measureNanoTime {
+                    foundPrime = cache.findPrime(startNumber)
+                })
+                cache.clear()
             }
-            cache.clear()
-        }
-        val dataList = timeMeasurements.toList()
-        val mean = Statistics.calculateMeanLong(dataList)
-        val sDev = Statistics.calculateStandardDeviation(dataList)
-        println("FindPrimeResult ($startNumber) : u=$mean, o-=$sDev")
+            printTestResults(
+                "TestFindPrime", "$startNumber, $foundPrime", timeMeasurements)
+        } } }
     }
-
 
 	@ParameterizedTest
 	@ValueSource(ints = [0, 4, 9, 12, 15, 16, 17, 18, 20,
@@ -38,10 +42,30 @@ class PrimeCachePerformanceTest {
     fun testTargetedAccess(target: Int) {
         val cache = BytePrimeCache()
         val prime = primes[target]
-        repeat(240_000) {
-            assertEquals(prime, cache.getPrime(target))
+        val timeMeasurements = ArrayList<Long>(10_000)
+        var foundPrime: Int? = null
+        for (i in 0 until 10_000) {
+            timeMeasurements.add(measureNanoTime {
+                foundPrime = cache.getPrime(target)
+            })
+            assertEquals(prime, foundPrime)
             cache.clear()
         }
+        printTestResults(
+            "TestTargetedAccess", "$target, $foundPrime", timeMeasurements)
+    }
+
+    /** Process and print a statistical summary of the data */
+    internal fun printTestResults(
+        testName: String, params: String, dataList: ArrayList<Long>
+    ) {
+        val max = timeMeasurements.maxOf {it}
+        timeMeasurements.removeIf {it == max}
+        val mean = Statistics.calculateMeanLong(dataList)
+        val sDev = Statistics.calculateStandardDeviation(dataList)
+        val min = dataList.minOrNull()
+        val max = dataList.maxOrNull()
+        println("$testName($params): u=$mean, o-=$sDev, min=$min, max=$max")
     }
 
 }
