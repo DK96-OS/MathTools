6f0ff4c8036020ac3f4b86266e5c168cdf457e1c
DK96 <69859316+DK96-OS@users.noreply.github.com>
2021-07-06 11:58:39 -0400

    Update PrimeCachePerformanceTest: * Create IsPrime Test case with a range of parameters * Add more Parameters to existing test cases to collect more data, doubling certain parameters with a high variance

--- Numbers/src/test/kotlin/mathtools/numbers/primes/PrimeCachePerformanceTest.kt
+++ Numbers/src/test/kotlin/mathtools/numbers/primes/PrimeCachePerformanceTest.kt
@@ -15,45 +15,68 @@ import kotlinx.coroutines.launch
 /** Performance Testing for Prime Number Caches */
 class PrimeCachePerformanceTest {
 
+    private val nDataPoints = 10_000
     private val primes: List<Int> = BytePrimeCacheTest.primeList
 
     @ParameterizedTest
     @ValueSource(ints = [9, 9, 15, 15, 21, 25, 25, 27, 29, 33, 35,
-    55, 55, 55, 71, 71, 71, 75, 75, 75, 77, 77, 77])
+        55, 55, 71, 71, 75, 75, 75, 77, 77, 91, 93, 95, 97, 183, 185,
+        187, 189, 191, 213, 215, 217, 219, 243, 245, 247, 249, 251,
+    ])
     fun testFindPrime(startNumber: Int) {
         runBlocking { coroutineScope { launch(Dispatchers.IO) {
             val cache = BytePrimeCache()
-            val timeMeasurements = ArrayList<Long>(10_000)
+            val timeList = ArrayList<Long>(nDataPoints)
             var foundPrime: Int? = null
-            for (i in 0 until 10_000) {
-                timeMeasurements.add(measureNanoTime {
+            for (i in 0 until nDataPoints) {
+                timeList.add(measureNanoTime {
                     foundPrime = cache.findPrime(startNumber)
                 })
                 cache.clear()
             }
             printTestResults(
-                "TestFindPrime", "$startNumber, $foundPrime", timeMeasurements)
+                "TestFindPrime", "$startNumber, $foundPrime", timeList)
         } } }
     }
 
+    @ParameterizedTest
+    @ValueSource(ints = [11, 11, 15, 15, 25, 25, 29, 29, 71, 73, 75, 109,
+        141, 143, 143, 145, 145, 147, 197, 199, 199, 201, 219, 221, 223,
+        241, 243, 245, 249, 251, 253, 255,
+    ])
+    fun testIsPrime(target: Int) {
+        val cache = BytePrimeCache()
+        val timeList = ArrayList<Long>(nDataPoints)
+        var wasPrime: Boolean? = null
+        for (i in 0 until nDataPoints) {
+            timeList.add(measureNanoTime {
+                wasPrime = cache.isPrime(target)
+            })
+            cache.clear()
+        }
+        printTestResults("TestIsPrime", "$target, $wasPrime", timeList)
+    }
+
 	@ParameterizedTest
-	@ValueSource(ints = [0, 4, 9, 12, 15, 16, 17, 18, 20,
-	22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 53])
+	@ValueSource(ints = [0, 4, 9, 12, 15, 16, 16, 16, 17, 17, 18, 20,
+	    22, 24, 24, 25, 26, 28, 30, 32, 34, 34, 36, 38, 40, 42, 44, 46,
+	    48, 50, 52, 53
+	])
     fun testTargetedAccess(target: Int) {
         runBlocking { coroutineScope { launch(Dispatchers.IO) {
             val cache = BytePrimeCache()
             val prime = primes[target]
-            val timeMeasurements = ArrayList<Long>(10_000)
+            val timeList = ArrayList<Long>(nDataPoints)
             var foundPrime: Int? = null
-            for (i in 0 until 10_000) {
-                timeMeasurements.add(measureNanoTime {
+            for (i in 0 until nDataPoints) {
+                timeList.add(measureNanoTime {
                     foundPrime = cache.getPrime(target)
                 })
                 assertEquals(prime, foundPrime)
                 cache.clear()
             }
             printTestResults(
-                "TestTargetedAccess", "$target, $foundPrime", timeMeasurements)
+                "TestTargetedAccess", "$target, $foundPrime", timeList)
         } } }
     }
 
@@ -69,7 +92,7 @@ class PrimeCachePerformanceTest {
         val sDev = Statistics.calculateStandardDeviation(dataList)
         val min = dataList.minOrNull()
         val max = dataList.maxOrNull()
-        println("$testName($params): u=$mean, o-=$sDev, min=$min, max=$max")
+        println("$testName($params): u=$mean, o-=$sDev, min=$min")
     }
 
 }
