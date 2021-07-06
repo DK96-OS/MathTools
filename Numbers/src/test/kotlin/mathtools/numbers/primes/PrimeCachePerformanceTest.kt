b4b01d711e4d3d8f09d658042ecb15dc7bc9b531
DK96 <69859316+DK96-OS@users.noreply.github.com>
2021-07-06 18:41:50 -0400

    Update PrimeCachePerformanceTest: * Remove Coroutines after determining that they have no visible impact on the test results * Cleanup unused imports, including removing assertions, as this test class is not concerned with correctness

--- Numbers/src/test/kotlin/mathtools/numbers/primes/PrimeCachePerformanceTest.kt
+++ Numbers/src/test/kotlin/mathtools/numbers/primes/PrimeCachePerformanceTest.kt
@@ -1,22 +1,16 @@
 package mathtools.numbers.primes
 
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.RepeatedTest
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 import mathtools.numbers.statistics.Statistics
 import kotlin.system.measureNanoTime
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
 
 /** Performance Testing for Prime Number Caches */
 class PrimeCachePerformanceTest {
 
+    /** The number of data points per test */
     private val nDataPoints = 10_000
-    private val primes: List<Int> = BytePrimeCacheTest.primeList
 
     @ParameterizedTest
     @ValueSource(ints = [9, 9, 15, 15, 21, 25, 25, 27, 29, 33, 35,
@@ -24,19 +18,17 @@ class PrimeCachePerformanceTest {
         187, 189, 191, 213, 215, 217, 219, 243, 245, 247, 249, 251,
     ])
     fun testFindPrime(startNumber: Int) {
-        runBlocking { coroutineScope { launch(Dispatchers.IO) {
-            val cache = BytePrimeCache()
-            val timeList = ArrayList<Long>(nDataPoints)
-            var foundPrime: Int? = null
-            for (i in 0 until nDataPoints) {
-                timeList.add(measureNanoTime {
-                    foundPrime = cache.findPrime(startNumber)
-                })
-                cache.clear()
-            }
-            printTestResults(
-                "TestFindPrime", "$startNumber, $foundPrime", timeList)
-        } } }
+        val cache = BytePrimeCache()
+        val timeList = ArrayList<Long>(nDataPoints)
+        var foundPrime: Int? = null
+        for (i in 0 until nDataPoints) {
+            timeList.add(measureNanoTime {
+                foundPrime = cache.findPrime(startNumber)
+            })
+            cache.clear()
+        }
+        printTestResults(
+            "TestFindPrime", "$startNumber, $foundPrime", timeList)
     }
 
     @ParameterizedTest
@@ -63,21 +55,17 @@ class PrimeCachePerformanceTest {
 	    48, 50, 52, 53
 	])
     fun testTargetedAccess(target: Int) {
-        runBlocking { coroutineScope { launch(Dispatchers.IO) {
-            val cache = BytePrimeCache()
-            val prime = primes[target]
-            val timeList = ArrayList<Long>(nDataPoints)
-            var foundPrime: Int? = null
-            for (i in 0 until nDataPoints) {
-                timeList.add(measureNanoTime {
-                    foundPrime = cache.getPrime(target)
-                })
-                assertEquals(prime, foundPrime)
-                cache.clear()
-            }
-            printTestResults(
-                "TestTargetedAccess", "$target, $foundPrime", timeList)
-        } } }
+        val cache = BytePrimeCache()
+        val timeList = ArrayList<Long>(nDataPoints)
+        var foundPrime: Int? = null
+        for (i in 0 until nDataPoints) {
+            timeList.add(measureNanoTime {
+                foundPrime = cache.getPrime(target)
+            })
+            cache.clear()
+        }
+        printTestResults(
+            "TestTargetedAccess", "$target, $foundPrime", timeList)
     }
 
     /** Process and print a statistical summary of the data */
@@ -91,7 +79,7 @@ class PrimeCachePerformanceTest {
         val mean = Statistics.calculateMeanLong(dataList)
         val sDev = Statistics.calculateStandardDeviation(dataList)
         val min = dataList.minOrNull()
-        val max = dataList.maxOrNull()
+        //val max = dataList.maxOrNull()
         println("$testName($params): u=$mean, o-=$sDev, min=$min")
     }
 
