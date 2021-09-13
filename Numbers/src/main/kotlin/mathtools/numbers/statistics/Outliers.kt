27fa5c6d90ee8fdf76490ba54c6d90fa13353a90
DK96 <69859316+DK96-OS@users.noreply.github.com>
2021-09-13 19:35:16 -0400

    Create Outliers utility functions

---
+++ Numbers/src/main/kotlin/mathtools/numbers/statistics/Outliers.kt
@@ -0,0 +1,49 @@
+package mathtools.numbers.statistics
+
+/** Outlier scanning functions
+* Developed by DK96-OS : 2021 */
+object Outliers {
+
+    /** Build a list of outliers
+      * Developed by DK96-OS : 2021 */
+    fun scanOutliers(
+        dc: DistributionCharacteristics, sDevLimit: Double) 
+    : ArrayList<Double>? {
+        val upperList = upperOutliers(dc, sDevLimit)
+        val lowerList = lowerOutliers(dc, sDevLimit)
+        return when {
+            upperList != null -> if (lowerList == null) 
+                upperList else upperList.addAll(lowerList)
+            lowerList != null -> lowerList
+            else -> null
+        }
+    }
+
+    /** Build a list of outliers above the given limit 
+      *  Developed by DK96-OS : 2021 */
+    fun upperOutliers(
+        dc: DistributionCharacteristics, sDevLimit: Double)
+    : ArrayList<Double>? {
+        val upperLimit = dc.mean + sDevLimit * dc.standardDeviation
+        return if (dc.max > upperLimit) {
+            val list = arrayListOf(dc.max)
+            //
+            list
+        } else null
+    }
+
+    /** Build a list of outliers below the given limit
+      * Developed by DK96-OS : 2021 */
+    fun lowerOutliers(
+        dc: DistributionCharacteristics, sDevLimit: Double)
+    : ArrayList<Double>? {
+        val lowerLimit = dc.mean - sDevLimit * dc.standardDeviation
+        return if (dc.min < lowerLimit) {
+            val list = arrayListOf(dc.min)
+            //
+            list
+        } else null
+    }
+
+}
+
