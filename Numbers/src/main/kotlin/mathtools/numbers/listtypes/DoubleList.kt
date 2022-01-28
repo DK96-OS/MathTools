89aa59337eb1276f65574085feba656473ec9977
DK96 <69859316+DK96-OS@users.noreply.github.com>
2022-01-28 17:11:40 -0500

    DoubleList: * Create findOutOfBounds method * Update docstrings

--- Numbers/src/main/kotlin/mathtools/numbers/listtypes/DoubleList.kt
+++ Numbers/src/main/kotlin/mathtools/numbers/listtypes/DoubleList.kt
@@ -5,8 +5,8 @@ import java.math.BigDecimal
 /** Functions of Double typed Lists */
 object DoubleList {
 
-    /** Find all elements greater than the limit
-     * @return Indices of found elements, or null if none */
+    /** Find indices of elements greater than the limit
+     * @return Indices of elements above limit */
     fun findGreaterThan(
         list: List<Double>,
         limit: Double,
@@ -26,8 +26,8 @@ object DoubleList {
         return indices ?: emptyList()
     }
 
-    /** Find all elements less than the limit
-     * @return Indices of found elements, or null if none */
+    /** Find indices of elements less than the limit
+     * @return Indices of elements below limit */
     fun findLessThan(
         list: List<Double>,
         limit: Double,
@@ -38,6 +38,28 @@ object DoubleList {
         var indices: ArrayList<Int>? = null
         for (idx in start until list.size) {
             if (list[idx] < limit) {
+                if (indices == null)
+                    indices = arrayListOf(idx)
+                else
+                    indices.add(idx)
+            }
+        }
+        return indices ?: emptyList()
+    }
+
+    /** Find indices of elements outside of the specified boundaries
+     * @return Indices of elements out of bounds */
+    fun findOutOfBounds(
+        list: List<Double>,
+        lowerBound: Double,
+        upperBound: Double,
+        start: Int = 0
+    ) : List<Int> {
+        if (start < 0 || list.isEmpty())
+            return emptyList()
+        var indices: ArrayList<Int>? = null
+        for (idx in start until list.size) {
+            if (list[idx] !in lowerBound .. upperBound) {
                 if (indices == null)
                     indices = arrayListOf(idx)
                 else
