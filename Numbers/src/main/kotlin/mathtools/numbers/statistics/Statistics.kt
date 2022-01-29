6ee976e465f475747c80d6ed3408c9df2aaecce0
DK96 <69859316+DK96-OS@users.noreply.github.com>
2022-01-29 06:57:37 -0500

    Statistics: * Update code style in Statistics object * Fix precision of float in oneIn function * Modify calculateMean function - add specific handling for lists of size 1 * Modify calculateSDev function - provide value for lists of size 2 instead of 0.0 * Update deprecation level to error

--- Numbers/src/main/kotlin/mathtools/numbers/statistics/Statistics.kt
+++ Numbers/src/main/kotlin/mathtools/numbers/statistics/Statistics.kt
@@ -7,19 +7,33 @@ import kotlin.math.sqrt
 object Statistics {
 
 	/** Return true once every x times this function is called */
-	fun oneIn(x:Int): Boolean = when {
-		x > 1 -> Math.random() < 0.99999999f / x
+	fun oneIn(
+		x: Int
+	) : Boolean = when {
+		x > 1 -> Math.random() < 0.9999999f / x
 		x == 1 -> true
 		else -> throw IllegalArgumentException()
 	}
 
-    fun <T : Number> calculateMean(list:List<T>)
-    : Double = if (list.isEmpty()) 0.0 else listSum(list) / list.size
+	/** Calculate the mean (average) value of the elements in a Number List */
+    fun <T : Number> calculateMean(
+	    list: List<T>
+    ) : Double = when (list.size) {
+		0 -> 0.0
+	    1 -> list[0].toDouble()
+	    in 2 .. 50 -> listSum(list) / list.size
+	    else -> // Prefer large sum
+		    listSum(list) / list.size
+    }
 
+	/** Calculate the standard deviation of the elements in a Number List
+	 * @param list The elements of a distribution
+	 * @param mean The mean value of the distribution. Uses calculateMean() as default */
     fun <T : Number> calculateSDev(
-        list:List<T>, mean:Double = calculateMean(list))
-    : Double {
-        if (list.size <3) return 0.0
+        list: List<T>,
+        mean: Double = calculateMean(list)
+    ) : Double {
+        if (list.size < 2) return 0.0
         var varianceSum = 0.0
         for (i in list.indices) {
             val dev = list[i].toDouble() - mean
@@ -28,18 +42,21 @@ object Statistics {
         return sqrt(varianceSum / (list.size - 1))
     }
 
+	/* Deprecated Functions */
+
 	/** Sums the numbers in the list and divides by the count */
 	@Deprecated(
 	    message = "Use the common List Number functions",
 	    replaceWith = ReplaceWith("calculateMean(list)"),
-	    level = DeprecationLevel.WARNING)
-	fun calculateMeanLong(list:List<Long>)
-	: Float = if (list.isEmpty()) 0f else list.sum() / list.size.toFloat()
+	    level = DeprecationLevel.ERROR)
+	fun calculateMeanLong(
+		list: List<Long>
+	) : Float = if (list.isEmpty()) 0f else list.sum() / list.size.toFloat()
 
     @Deprecated(
 	    message = "Use the common List Number functions",
         replaceWith = ReplaceWith("calculateMean(list)"),
-        level = DeprecationLevel.WARNING)
+        level = DeprecationLevel.ERROR)
 	fun calculateMeanFloat(list:List<Float>)
 	: Float = if (list.isEmpty()) 0f else list.sum() / list.size.toFloat()
 
@@ -47,7 +64,7 @@ object Statistics {
 	@Deprecated(
 	    message = "Use the common List Number functions",
 	    replaceWith = ReplaceWith("calculateSDev"),
-        level = DeprecationLevel.WARNING)
+        level = DeprecationLevel.ERROR)
 	fun calculateStandardDeviation(
 		list:List<Long>, mean:Float = calculateMean(list).toFloat()) 
     : Float = calculateSDev(list, mean.toDouble()).toFloat()
@@ -56,7 +73,7 @@ object Statistics {
 	@Deprecated(
 	    message = "Use the common List Number functions",
 	    replaceWith = ReplaceWith("calculateSDev"),
-	    level = DeprecationLevel.WARNING)
+	    level = DeprecationLevel.ERROR)
 	fun calculateStandardDevFloat(
 		list:List<Float>, mean:Float = calculateMean(list).toFloat()) 
     : Float = calculateSDev(list, mean.toDouble()).toFloat()
