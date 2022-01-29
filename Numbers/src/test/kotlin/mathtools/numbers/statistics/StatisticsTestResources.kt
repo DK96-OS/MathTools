2fae8b3d86ed1f440597e3ccfefce91d75a5320b
DK96 <69859316+DK96-OS@users.noreply.github.com>
2022-01-29 08:11:58 -0500

    Tests: * Create StatisticsTestResources - provide Number lists and distribution characteristics for different cases

---
+++ Numbers/src/test/kotlin/mathtools/numbers/statistics/StatisticsTestResources.kt
@@ -0,0 +1,44 @@
+package mathtools.numbers.statistics
+
+import mathtools.numbers.statistics.DistributionCharacteristics.Companion.process
+import kotlin.math.PI
+import kotlin.math.cos
+
+object StatisticsTestResources {
+
+	val uniform101: List<Number> = ByteArray(101) {
+		(it - 20).toByte()
+	}.toList()
+
+	val uniform101DC = process(uniform101)!!
+
+	val uniform1001: List<Number>
+		get() = ShortArray(1001) {
+			(it - 200).toShort()
+		}.toList()
+
+	val uniform10001: List<Number>
+		get() = ShortArray(10001) {
+			(it - 2000).toShort()
+		}.toList()
+
+	val precise101: List<Number> = FloatArray(101) {
+		50f + ((it / 50f) - 1)
+	}.toList()
+
+	val twoPi = (2 * PI).toFloat()
+	val twoPi101 = twoPi / 101f
+
+	val wave101: List<Number> = FloatArray(101) {
+		cos( twoPi101 * it)
+	}.toList()
+
+	val wave101DC = process(wave101)!!
+
+	fun convertToDouble(list: List<Number>)
+	: MutableList<Double> = (list as List<Double>).toMutableList()
+
+	fun convertToLong(list: List<Number>)
+	: MutableList<Long> = (list as List<Long>).toMutableList()
+
+}
