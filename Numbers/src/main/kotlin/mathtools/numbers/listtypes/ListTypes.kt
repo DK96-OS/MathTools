3e244c8873f1b016c370f0ad3b7f670c88b7cefc
DK96 <69859316+DK96-OS@users.noreply.github.com>
2022-01-29 09:44:08 -0500

    Tests: * Deprecate listSum function in ListTypes.kt

--- Numbers/src/main/kotlin/mathtools/numbers/listtypes/ListTypes.kt
+++ Numbers/src/main/kotlin/mathtools/numbers/listtypes/ListTypes.kt
@@ -2,17 +2,22 @@ package mathtools.numbers.listtypes
 
 /** Sum up the numbers in the list
  * Use with caution, overflows are not checked in most cases. */
-fun <T : Number> listSum(list: List<T>)
-: Double = when (list.firstOrNull()) {
+@Deprecated(
+    message = "Incomplete and imprecise function. For casual users.",
+    level = DeprecationLevel.WARNING
+)
+fun <T : Number> listSum(
+    list: List<T>
+) : Double = when (list.firstOrNull()) {
     is Double -> (list as List<Double>).sum()
     is Float -> {
         val floatList = list as List<Float>
         var sum = 0.0
-        for (i in floatList.indices)
-            sum += floatList[i].toDouble()
+        for (i in floatList.indices) sum += floatList[i]
         sum
     }
-    is Long -> LongList.largeSum(list as List<Long>).toDouble()
+    is Long -> LongList.largeSum(list as List<Long>)
+        .toDouble()
     is Int -> {
         val intList = list as List<Int>
         var sum = 0L
@@ -31,5 +36,5 @@ fun <T : Number> listSum(list: List<T>)
         for (i in byteList.indices) sum += byteList[i]
         sum.toDouble()
     }
-    else -> 0.0
+    else -> throw IllegalArgumentException("Unsupported Type")
 }
