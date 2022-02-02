c42f2c9320d3c1fb1b0beb1c40e9e843aa34f2f9
DK96 <69859316+DK96-OS@users.noreply.github.com>
2022-02-02 14:36:50 -0500

    Numbers (Generator): * Remove Generator, ProductGenerator, and outcome stats tests

--- numbers/src/main/kotlin/mathtools/numbers/generators/Generator.kt
+++
@@ -1,9 +0,0 @@
-package mathtools.numbers.generators
-
-/** Defines a reusable numerical data generator */
-interface Generator {
-
-    /** Performs a data Generator cycle, updating member data */
-    fun generate()
-
-}
