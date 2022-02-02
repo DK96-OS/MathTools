752aab25a6f0d2dd875babf5dc17b23ad212801e
DK96 <69859316+DK96-OS@users.noreply.github.com>
2022-02-02 06:30:42 -0500

    Tests: * Create DeviationPolicyIdentifyOutliersTest - provide fundamental test cases

---
+++ Numbers/src/test/kotlin/mathtools/numbers/statistics/outlier/DeviationPolicyIdentifyOutliersTest.kt
@@ -0,0 +1,100 @@
+package mathtools.numbers.statistics.outlier
+
+import mathtools.numbers.statistics.outlier.DeviationPolicyTestResources.policy1SD
+import mathtools.numbers.statistics.outlier.DeviationPolicyTestResources.policy1SDLower
+import mathtools.numbers.statistics.outlier.DeviationPolicyTestResources.policy1SDUpper
+import mathtools.numbers.statistics.outlier.DeviationPolicyTestResources.policy2SD
+import mathtools.numbers.statistics.outlier.DeviationPolicyTestResources.policy2SDLower
+import mathtools.numbers.statistics.outlier.DeviationPolicyTestResources.policy2SDUpper
+import mathtools.numbers.testdata.UniformTestDataSource.uniform101
+import mathtools.numbers.testdata.UniformTestDataSource.uniform101DC
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+/** Testing DeviationPolicy's IdentifyOutliers method
+ * Developed by DK96-OS : 2022 */
+class DeviationPolicyIdentifyOutliersTest {
+
+	private val doubleArray = DoubleArray(uniform101.size) {
+		uniform101[it].toDouble()
+	}
+	private val longArray = LongArray(uniform101.size) {
+		uniform101[it].toLong()
+	}
+
+	@Test
+	fun testNoOutliersDouble() {
+		assertEquals(
+			0, policy2SD.identifyOutliers(
+				doubleArray, uniform101DC
+			).size
+		)
+		assertEquals(
+			0, policy2SDUpper.identifyOutliers(
+				doubleArray, uniform101DC
+			).size
+		)
+		assertEquals(
+			0, policy2SDLower.identifyOutliers(
+				doubleArray, uniform101DC
+			).size
+		)
+	}
+
+	@Test
+	fun testNoOutliersLong() {
+		assertEquals(
+			0, policy2SD.identifyOutliers(
+				longArray, uniform101DC
+			).size
+		)
+		assertEquals(
+			0, policy2SDUpper.identifyOutliers(
+				longArray, uniform101DC
+			).size
+		)
+		assertEquals(
+			0, policy2SDLower.identifyOutliers(
+				longArray, uniform101DC
+			).size
+		)
+	}
+
+	@Test
+	fun test1SDDouble() {
+		var result = policy1SD.identifyOutliers(
+			doubleArray, uniform101DC
+		)
+		assertEquals(42, result.size)
+		//
+		result = policy1SDUpper.identifyOutliers(
+			doubleArray, uniform101DC
+		)
+		assertEquals(21, result.size)
+		//
+		result = policy1SDLower.identifyOutliers(
+			doubleArray, uniform101DC
+		)
+		assertEquals(21, result.size)
+	}
+
+	@Test
+	fun test1SDLong() {
+		var result = policy1SD.identifyOutliers(
+			longArray, uniform101DC
+		)
+		assertEquals(42, result.size)
+		//
+		result = policy1SDUpper.identifyOutliers(
+			longArray, uniform101DC
+		)
+		assertEquals(21, result.size)
+		//
+		result = policy1SDLower.identifyOutliers(
+			longArray, uniform101DC
+		)
+		assertEquals(21, result.size)
+
+	}
+
+}
