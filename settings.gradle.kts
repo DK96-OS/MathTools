5e2a9a947dd9b09fee24fd1f3e16707df1f84f85
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2022-02-12 14:10:49 -0500

    Aggregated Test Report (#45)
    Build Updates:
    * Add TestReportAggregation plugin to Numbers module
    * Limit number of gradle workers to 2
    * Make the Check task depend on TestAggregateTestReport task
    * Update Gradle 7.3.3 -> 7.4
    * Add gradlew scripts
    
    Build and Test Workflow:
    * Remove cache action
    * Remove individual module test report artifacts
    * Upload Aggregated Test Report
    * Use gradle-build-action @5a719 to manage caching and gradle-version
    
    Publish Release Workflow:
    * Remove cache action
    * Use gradle-build-action @5a719 to manage caching and gradle-version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 /*
  * The settings file is used to specify which projects to include in your build
  * Detailed information about configuring a multi-project build in Gradle can be found
- * in the user manual at https://docs.gradle.org/6.8.3/userguide/multi_project_builds.html
+ * in the user manual at https://docs.gradle.org/7.4/userguide/multi_project_builds.html
  */
  
 pluginManagement {
