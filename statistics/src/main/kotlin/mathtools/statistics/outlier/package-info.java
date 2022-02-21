15eaaa341f1c7bda3caf6662cf071918352c9244
DK96 <69859316+DK96-OS@users.noreply.github.com>
2022-02-21 06:29:13 -0500

    Statistics: * Add package-info.java to Statistics packages

---
+++ statistics/src/main/kotlin/mathtools/statistics/outlier/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Outlier Detection and Removal Operations and Data Structures
+ * @author DK96-OS
+ */
+package mathtools.statistics.outlier;
---
+++ statistics/src/main/kotlin/mathtools/statistics/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Statistical Operations and Data Structures
+ * @author DK96-OS
+ */
+package mathtools.statistics;
