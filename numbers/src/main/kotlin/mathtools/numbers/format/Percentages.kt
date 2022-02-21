15c931be3903bba803bf1e89d50407bd4c77fa9f
DK96 <69859316+DK96-OS@users.noreply.github.com>
2022-02-21 05:52:02 -0500

    Numbers (Format): * Update Percentages - refactor strictPercent function * Update PercentagesTest - add more test cases

--- numbers/src/main/kotlin/mathtools/numbers/format/Percentages.kt
+++ numbers/src/main/kotlin/mathtools/numbers/format/Percentages.kt
@@ -1,6 +1,6 @@
 package mathtools.numbers.format
 
-import mathtools.numbers.factors.IntOperations
+import mathtools.numbers.factors.IntOperations.tenShift
 import kotlin.math.roundToInt
 
 /** Handling Percentage Values and Formatting
@@ -9,19 +9,24 @@ object Percentages {
 
 	/** Format a float as a percent string with specific accuracy.
 	 *  Valid outputs are between 0% and 100% - why it is called 'strict'
+	 *  Up to 5 decimal places are allowed.
 	 * @param f Should be within 0f and 1f, otherwise the closest value of 0% or 100% is returned.
-	 * @param decimalsAllowed The number of decimal places to allow in the final string, 0 .. 6 */
+	 * @param decimalsAllowed The number of decimal places to allow in the final string, 0 .. 5 */
 	fun strictPercent(
 		f: Float,
 		decimalsAllowed: Byte = 0,
 	) : String = when {
-		f in 0f .. 1f -> if (decimalsAllowed <= (0).toByte())
-			(f * 100).roundToInt().toString().plus("%")
-		else if (decimalsAllowed in 1 .. 7) {
-			val shift = IntOperations.tenShift(1, decimalsAllowed.toInt())
-			val rounded = (f * 100 * shift).roundToInt().toFloat()
-			(rounded / shift).toString().plus("%")
-		} else throw IllegalArgumentException("Invalid Decimal count")
+		f in 0.0000001f .. 1f -> when {
+			decimalsAllowed < 1 -> (f * 100).roundToInt()
+			decimalsAllowed < 5 -> {
+				val shift = tenShift(1, decimalsAllowed.toInt()).toFloat()
+				(f * 100f * shift).roundToInt() / shift
+			}
+			else -> {
+				val shift = tenShift(1, 5).toFloat()
+				(f * 100f * shift).roundToInt() / shift
+			}
+		}.toString().plus("%")
 		f > 1f -> "100%"
 		else -> "0%"
 	}
--- numbers/src/test/kotlin/mathtools/numbers/format/PercentagesTest.kt
+++ numbers/src/test/kotlin/mathtools/numbers/format/PercentagesTest.kt
@@ -29,6 +29,27 @@ class PercentagesTest {
 	}
 
 	@Test
+	fun testStrictPercent3Decimal() {
+		assertEquals("1.01%", strictPercent(0.0101f, 3))
+		assertEquals("2.575%", strictPercent(0.02575f, 3))
+		assertEquals("20.0%", strictPercent(0.200f, 3))
+		assertEquals("21.111%", strictPercent(0.21111f, 3))
+		assertEquals("30.301%", strictPercent(0.303006f, 3))
+		assertEquals("31.778%", strictPercent(0.317777f, 3))
+	}
+
+	@Test
+	fun testStrictPercent7Decimal() {
+		// Note that 7 is higher than allowed, coerced down to 5
+		assertEquals("1.0101%", strictPercent(0.010101f, 7))
+		assertEquals("2.57503%", strictPercent(0.02575025f, 7))
+		assertEquals("20.0%", strictPercent(0.200000f, 7))
+		assertEquals("21.111%", strictPercent(0.21111f, 7))
+		assertEquals("30.30104%", strictPercent(0.3030104f, 7))
+		assertEquals("31.7777%", strictPercent(0.317777f, 7))
+	}
+
+	@Test
 	fun testStrictPercentNegative() {
 		assertEquals("0%", strictPercent(-0.01f, 0))
 		assertEquals("0%", strictPercent(-0.02f, 0))
@@ -39,6 +60,13 @@ class PercentagesTest {
 	fun testStrictPercentNegativeDecimals() {
 		assertEquals("1%", strictPercent(0.01f, -1))
 		assertEquals("3%", strictPercent(0.03f, -3))
+	}
+
+	@Test
+	fun testStrictPercentSpecialValues() {
+		assertEquals("0%", strictPercent(Float.NaN, 0))
+		assertEquals("100%", strictPercent(Float.POSITIVE_INFINITY, 0))
+		assertEquals("0%", strictPercent(Float.NEGATIVE_INFINITY, 0))
 	}
 
 }
