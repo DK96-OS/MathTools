31f2bed3b50347c47c5f567a8a3252d547832860
DK96 <69859316+DK96-OS@users.noreply.github.com>
2022-02-22 18:14:54 -0500

    Lists: * Update ShortArrayExt.java - add docs

--- lists/src/main/java/mathtools/lists/arrays/ShortArrayExt.java
+++ lists/src/main/java/mathtools/lists/arrays/ShortArrayExt.java
@@ -1,9 +1,15 @@
 package mathtools.lists.arrays;
 
+/** Methods for operating on a short array
+ * @author DK96-OS : 2022 */
 public class ShortArrayExt {
 
     private ShortArrayExt() {}
 
+    /** Calculate the sum
+     *  Does not prevent 64-bit long overflow
+     * @param array The elements to add in the sum
+     * @return A long integer, the sum of the elements */
     public static long sum(short[] array) {
         long sum = 0;
         for (short s : array) sum += s;
