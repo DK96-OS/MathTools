7083a0fba02b7338fa6c077f13ba5d0b9217e022
DK96 <69859316+DK96-OS@users.noreply.github.com>
2022-02-21 21:04:15 -0500

    Lists: * Update ByteArrayExt.java - add clear method

--- lists/src/main/java/mathtools/lists/arrays/ByteArrayExt.java
+++ lists/src/main/java/mathtools/lists/arrays/ByteArrayExt.java
@@ -1,13 +1,28 @@
 package mathtools.lists.arrays;
 
+/** Methods for operating on a byte array
+ * @author DK96-OS : 2022 */
 public class ByteArrayExt {
 
     private ByteArrayExt() {}
 
+    /** Compute the sum of all elements in the array
+     * @param array The array of elements
+     * @return A Long value containing the sum of the elements */
     public static long sum(byte[] array) {
         long sum = 0;
         for (byte b : array) sum += b;
         return sum;
+    }
+
+    /** Clears any nonzero elements in the Array
+     * @param array The array to reset to all zero */
+    public static void clear(
+            byte[] array
+    ) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != 0) array[i] = 0;
+        }
     }
 
 }
