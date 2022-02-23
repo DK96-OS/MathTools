f08ca19d9be49695d9bbd4a275f44c1a200bce67
DK96 <69859316+DK96-OS@users.noreply.github.com>
2022-02-22 19:07:46 -0500

    Lists (arrays): * Create LongArrayExt.java * Create IntArrayExt.java * Update ShortArrayExt.java - make class final * Update ByteArrayExt.java - make class final

--- lists/src/main/java/mathtools/lists/arrays/IntArrayExt.java
+++ lists/src/main/java/mathtools/lists/arrays/IntArrayExt.java
@@ -2,21 +2,21 @@ package mathtools.lists.arrays;
 
 import org.jetbrains.annotations.NotNull;
 
-/** Methods for operating on a short array
+/** Methods for operating on an int array
  * @author DK96-OS : 2022 */
-public final class ShortArrayExt {
+public final class IntArrayExt {
 
-    private ShortArrayExt() {}
+    private IntArrayExt() {}
 
     /** Calculate the sum
      *  Does not prevent 64-bit long overflow
      * @param array The elements to add in the sum
      * @return A long integer, the sum of the elements */
     public static long sum(
-            @NotNull short[] array
+            @NotNull int[] array
     ) {
         long sum = 0;
-        for (short s : array) sum += s;
+        for (int s : array) sum += s;
         return sum;
     }
 
