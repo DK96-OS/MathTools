c76754a647ae323747de37a7e6f79f79b6adf932
DK96 <69859316+DK96-OS@users.noreply.github.com>
2022-03-04 11:50:29 -0500

    Generators: * Create IntGeneratorCounter.java

---
+++ generators/src/main/java/mathtools/generators/counters/ints/IntGeneratorCounter.java
@@ -0,0 +1,43 @@
+package mathtools.generators.counters.ints;
+
+import javax.annotation.Nullable;
+
+import mathtools.generators.elements.ints.IntElementInterface;
+
+/** Runs an Integer Element Generator alongside a Counter
+ * @author DK96-OS : 2022 */
+public class IntGeneratorCounter {
+
+    private final IntElementInterface mGenerator;
+    private final IntCounterInterface mCounter;
+
+    private Integer mInvalidValue = null;
+
+    public IntGeneratorCounter(
+            IntElementInterface generator,
+            IntCounterInterface counter
+    ) {
+        mGenerator = generator;
+        mCounter = counter;
+    }
+
+    /** Run the [IntElementInterface] a number of times,
+     *  counting the outcomes using [IntCounterInterface]
+     * @param n The number of times to generate from the [IntElementInterface]
+     * @return True if the [IntCounterInterface] counted all values successfully
+     */
+    public boolean countGeneratedValues(int n) {
+        for (int i = 0; i < n; i++) {
+            int value = mGenerator.generate();
+            if (!mCounter.count(value)) {
+                mInvalidValue = value;
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Nullable
+    public Integer getRejectedValue() { return mInvalidValue; }
+
+}
