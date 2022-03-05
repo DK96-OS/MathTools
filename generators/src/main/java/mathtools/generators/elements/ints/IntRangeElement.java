2f11601eda1abdcd52335214d901cde2a1983aa0
DK96 <69859316+DK96-OS@users.noreply.github.com>
2022-03-05 09:05:38 -0500

    Generators: * Update IntRangeElement - add documentation, improve code style

--- generators/src/main/java/mathtools/generators/elements/ints/IntRangeElement.java
+++ generators/src/main/java/mathtools/generators/elements/ints/IntRangeElement.java
@@ -8,41 +8,47 @@ import com.google.common.math.IntMath;
 public class IntRangeElement implements IntElementInterface {
 
     /** The lowest integer in the range (inclusive) */
-    private int start;
+    private int mStart;
 
     /** The largest integer in the range (inclusive) */
-    private int end;
+    private int mEnd;
 
-    /**  */
-    private int bound;
+    /** A key state parameter that determines the generator behavior.
+     * Values of 0, 1 and 2 are handled specifically.
+     * When positive, is used as an argument in Random.nextInt(bound).
+     * When negative, integers are generated until one in range appears. */
+    private int mBound;
 
+    /** Validates start and end values, will reverse them to produce positive range
+     * @param start The first value in the range
+     * @param end The last value in the range */
     public IntRangeElement(
-            int start, int end
+            final int start,
+            final int end
     ) {
         if (start <= end) {
-            this.start = start;
-            this.end = end;
+            mStart = start;
+            mEnd = end;
         } else {
-            this.start = end;
-            this.end = start;
+            mStart = end;
+            mEnd = start;
         }
-        bound = calculateBound(this.start, this.end);
+        mBound = calculateBound(mStart, mEnd);
     }
 
     @Override
     public int generate() {
-        if (2 < bound)
-            return start + random.nextInt(bound);
-        switch (bound) {
-            case 0: return start == end ? start : random.nextInt();
+        if (2 < mBound)
+            return mStart + random.nextInt(mBound);
+        switch (mBound) {
+            case 0: return mStart == mEnd ? mStart : random.nextInt();
             case 1: case 2:
-                return random.nextBoolean() ? start : end;
+                return random.nextBoolean() ? mStart : mEnd;
         }
         // Bound is negative
-        // todo: Improve this later
         int trial = random.nextInt();
         byte counter = 0;
-        while (!(start <= trial && trial <= end)) {
+        while (!(mStart <= trial && trial <= mEnd)) {
             trial = random.nextInt();
             if (++counter > 31) break;
         }
@@ -53,30 +59,32 @@ public class IntRangeElement implements IntElementInterface {
      * Does not accept ranges where start is greater than end.
      * @return True if the range was able to be updated. false if no change occurred */
     public boolean setRange(
-            int start, int end
+            final int start,
+            final int end
     ) {
-        if (start > end || this.start == start && this.end == end)
+        if (start > end || mStart == start && mEnd == end)
             return false;
-        this.start = start;
-        this.end = end;
-        this.bound = calculateBound(start, end);
+        mStart = start;
+        mEnd = end;
+        mBound = calculateBound(start, end);
         return true;
     }
 
     /** The lowest integer in the range (inclusive) */
-    public int getStart() { return start; }
+    public int getStart() { return mStart; }
 
     /** The largest integer in the range (inclusive) */
-    public int getEnd() { return end; }
+    public int getEnd() { return mEnd; }
 
-    int getBound() { return bound; }
+    int getBound() { return mBound; }
 
     /** Determine the value to store as bound
      * @param start The first number in the Range
      * @param end The last number in the Range
      * @return A value representing the state of this element */
     static int calculateBound(
-            int start, int end
+            final int start,
+            final int end
     ) {
         if (end < start) return calculateBound(end, start);
         if (start == end) return 0;
@@ -87,7 +95,7 @@ public class IntRangeElement implements IntElementInterface {
                 return 0;   // Full Range of integers
         }
         try {
-            int diff = IntMath.checkedSubtract(end, start);
+            final int diff = IntMath.checkedSubtract(end, start);
             return IntMath.checkedAdd(1, diff);
         } catch (ArithmeticException e) {
             return -1;
