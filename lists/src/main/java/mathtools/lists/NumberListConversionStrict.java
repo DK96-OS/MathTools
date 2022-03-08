36c28bd6fc401e36498f9f2e5616d51a79e75466
DK96 <69859316+DK96-OS@users.noreply.github.com>
2022-03-08 09:23:36 -0500

    Lists: * Update NumberListConversionStrict - add documentation, improve code style, create toShortStrict method (similar to toByteStrict method)

--- lists/src/main/java/mathtools/lists/NumberListConversionStrict.java
+++ lists/src/main/java/mathtools/lists/NumberListConversionStrict.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import javax.annotation.Nonnull;
+
 /** Numbered List Conversion functions that are strict
  * @author DK96-OS : 2022 */
 public final class NumberListConversionStrict {
@@ -14,10 +16,12 @@ public final class NumberListConversionStrict {
 
     /** Attempts to convert to a list of bytes
      * @param list The input list to convert
-     * @param ignoreInvalid Whether to ignore invalid conversions, or throw exception */
+     * @param ignoreInvalid Whether to ignore errors, or throw exception
+     * @return A list of Bytes that were converted exactly */
+    @Nonnull
     public static List<Byte> toByteStrict(
-            List<Number> list,
-            boolean ignoreInvalid
+            @Nonnull final List<Number> list,
+            final boolean ignoreInvalid
     ) throws NumberFormatException {
         if (list.isEmpty())
             return Collections.emptyList();
@@ -29,6 +33,30 @@ public final class NumberListConversionStrict {
             else if (!ignoreInvalid)
                 throw new NumberFormatException(
                         "Cannot convert " + number + " to byte"
+                );
+        }
+        return newList;
+    }
+
+    /** Attempts to convert to a list of shorts
+     * @param list The input list to convert
+     * @param ignoreInvalid Whether to ignore errors, or throw exception
+     * @return A list of Short that were converted exactly */
+    @Nonnull
+    public static List<Short> toShortStrict(
+            @Nonnull final List<Number> list,
+            final boolean ignoreInvalid
+    ) throws NumberFormatException {
+        if (list.isEmpty())
+            return Collections.emptyList();
+        final ArrayList<Short> newList = new ArrayList<>(list.size());
+        for (Number number : list) {
+            final short b = number.shortValue();
+            if (isEquivalent(number, b))
+                newList.add(b);
+            else if (!ignoreInvalid)
+                throw new NumberFormatException(
+                        "Cannot convert " + number + " to short"
                 );
         }
         return newList;
