2116c8e19279dc835328f028758a899c0fd933b1
DK96 <69859316+dk96-os@users.noreply.github.com>
2022-03-15 00:32:46 -0400

    Numbers: * Remove PrimeCacheBase.kt * Update BytePrimeCache - implement PrimeCacheInterface and PrimeChecker * Update ShortPrimeCache - implement PrimeCacheInterface and PrimeChecker

--- numbers/src/main/kotlin/mathtools/numbers/primes/BytePrimeCache.kt
+++ numbers/src/main/kotlin/mathtools/numbers/primes/BytePrimeCache.kt
@@ -1,22 +1,27 @@
 package mathtools.numbers.primes
 
 /** An array-based cache for Prime Numbers in the 8-bit integer range
- * @author DK96-OS : 2021 */
-open class BytePrimeCache : PrimeCacheBase(
-	maxIndex = MAX_INDEX,
-	maxValue = MAX_PRIME,
-) {
+ * @author DK96-OS : 2021 - 2022 */
+open class BytePrimeCache
+	: PrimeCacheInterface {
+
 	private var byteArray: ByteArray? = null
 	private val byteQueue = ArrayList<UByte>(8)
 
 	internal val arraySize: Int get() = byteArray?.size ?: 0
 	internal val queueSize: Int get() = byteQueue.size
 
-    override fun highestCachedIndex()
+	override fun getMaxIndex(): Int = MAX_INDEX
+
+	override fun getMaxPrime(): Int = MAX_PRIME
+
+	override fun getHighestCachedIndex()
 		: Int = 15 + (byteArray?.size ?: 0) + byteQueue.size
 
-    override fun getPrime(idx: Int): Int = when {
-        idx < 16 -> initArray[idx].toInt()
+    override fun getPrime(
+	    idx: Int
+    ) : Int = when {
+        idx < 16 -> PrimeChecker.getStaticPrime(idx)
         byteArray != null -> {
             val arrayIndex = idx - 16
             val arraySize = byteArray!!.size
@@ -36,20 +41,25 @@ open class BytePrimeCache : PrimeCacheBase(
         else -> throw IllegalStateException("Could not get prime at: $idx")
     }
 
+	/** Determine if a number is prime */
+	fun isPrime(number: Int)
+		: Boolean = PrimeChecker.isPrime(number, this)
+
 	/** Find new primes and add to the cache, up to the given index
 	  * @param toIndex The Index to extend the cache to */
-	private fun extendCache(toIndex: Int)
-	: Boolean = if (toIndex in indexRange) {
-		var primesRequired = toIndex - highestCachedIndex()
+	private fun extendCache(
+		toIndex: Int
+	) : Boolean = if (toIndex in 0 .. maxIndex) {
+		var primesRequired = toIndex - highestCachedIndex
 		if (primesRequired <= 0) false
 		else if (byteQueue.size + primesRequired > 8)
 		    consolidate(primesRequired) > 0
 		else {		// Add to queue
-			var prevPrime = getPrime(highestCachedIndex())
+			var prevPrime = getPrime(highestCachedIndex)
 			var testN = prevPrime + 2
 			while (primesRequired > 0) {
-				val prime = findPrime(testN) ?: break
-				if (prime > maxValue) break
+				val prime = PrimeChecker.findPrime(testN, this) ?: break
+				if (prime > maxPrime) break
 				byteQueue.add(prime.toUByte())
 				testN = prime + if (prime - prevPrime == 2) 4 else 2
 				prevPrime = prime
@@ -59,18 +69,22 @@ open class BytePrimeCache : PrimeCacheBase(
 		}
 	} else false
 
-	override fun consolidate(add: Int): Int {
+	private fun consolidate(
+		add: Int
+	) : Int {
 		if (add <= 0) throw IllegalArgumentException()
 		val prevSize = arraySize + byteQueue.size
-		if (prevSize + add > indexRange.last + 1) return -1
+		//
+		if (prevSize + add > maxIndex + 1) return -1
 		val oldArray = byteArray
-		var prev: Int = getPrime(highestCachedIndex())
+		var prev: Int = getPrime(highestCachedIndex)
+		//
 		byteArray = ByteArray(prevSize + add) {
 			when {
 				it < (oldArray?.size ?: 0) -> oldArray!![it]
 				it < prevSize -> byteQueue.removeFirst().toByte()
 				else -> {
-					prev = findPrime(prev + 2)
+					prev = PrimeChecker.findPrime(prev + 2, this)
 					       ?: throw IllegalStateException("Next prime not found")
 					prev.toByte()
 				}
