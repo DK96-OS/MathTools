6f4e013d79a24838ebf00badca098a49e00117b1
DK96 <69859316+dk96-os@users.noreply.github.com>
2022-03-18 15:09:23 -0400

    Numbers: * Convert NumberFactors to Java * Update NumberFactorsCountFactorTest

--- numbers/src/main/java/mathtools/numbers/factors/NumberFactors.java
+++ numbers/src/main/java/mathtools/numbers/factors/NumberFactors.java
@@ -16,6 +16,43 @@ public final class NumberFactors {
 		return d == 5 || d == 0 && length > 0;
 	}
 
+	/** Count the power of a factor in a composite number.
+	 *  Ignores negative signs.
+	 * @param factor The factor to check for
+	 * @param composite The number to check in
+	 * @return The power of factor in the composite */
+	public static int countFactor(
+		final int factor,
+		final long composite
+	) {
+		// validate factor
+		if (factor < 0)
+			return countFactor(-factor, composite);
+		else if (factor < 2)
+			return 0;
+		// validate composite
+		else if (composite < 0L)
+			return countFactor(factor, -composite);
+		else if (composite < 2)
+			return 0;
+		else {
+			final long longF = factor;    // cast to long once
+			long remainder = composite / longF;
+			// check by multiplication
+			final long reverse = remainder * longF;
+			// Assume always less than if fails
+			if (reverse < composite)
+				return 0;
+			else {
+				int counter = 1;
+				for(; remainder > 1L && remainder % longF == 0L; ++counter) {
+					remainder /= longF;
+				}
+				return counter;
+			}
+		}
+	}
+
 	private NumberFactors() {}
 
 }
