d3fc536371f8f7465ac6f0d15237986550c34a8a
DK96 <69859316+dk96-os@users.noreply.github.com>
2022-03-19 11:13:01 -0400

    Numbers: * Update Percentages.java - fix behavior when zero decimals are allowed

--- numbers/src/main/java/mathtools/numbers/format/Percentages.java
+++ numbers/src/main/java/mathtools/numbers/format/Percentages.java
@@ -11,6 +11,7 @@ public final class Percentages {
 	/** Format a float as a percent string with specific accuracy.
 	 *  Valid outputs are between 0% and 100% - why it is called 'strict'
 	 *  Up to 5 decimal places are allowed.
+	 *  If the number of decimals is non-zero, the string will always end in ".0%"
 	 * @param number Should be within 0 and 1, otherwise the closest value of 0% or 100% is returned.
 	 * @param decimalsAllowed The number of decimal places to allow in the final string, 0 .. 5
 	 * 	 * @return A string containing a percentage formatted number */
@@ -20,7 +21,10 @@ public final class Percentages {
 		final byte decimalsAllowed
 	) {
 		if (0.000_000_1f <= number && 1 > number)
-			return round(number * 100, decimalsAllowed) + "%";
+			if (1 > decimalsAllowed)
+				return Math.round(number * 100) + "%";
+			else
+				return round(number * 100, decimalsAllowed) + "%";
 		else
 			return (1f <= number) ? "100%" : "0%";
 	}
