194d84fd69db210cbb9a3dc98072aac78a75820e
DK96 <69859316+dk96-os@users.noreply.github.com>
2022-03-27 11:57:58 -0400

    Code Cleanup: * Update BitFactoring - improve code style * Update BitFactoringTest - add test cases

--- numbers/src/main/java/mathtools/numbers/factors/BitFactoring.java
+++ numbers/src/main/java/mathtools/numbers/factors/BitFactoring.java
@@ -4,24 +4,24 @@ package mathtools.numbers.factors;
  * @author DK96-OS : 2022 */
 public final class BitFactoring {
 
+    /** Determines whether a number contains a factor of 2
+     * @param number the Number to check
+     * @return True if number is divisible by 2 */
+    public static boolean isProductOf2(
+        final long number
+    ) {
+        return 1 < (number & -number);
+    }
+
+    /** Determines whether a number contains a factor of 2
+     * @param number the Number to check
+     * @return True if number is divisible by 2 */
+    public static boolean isProductOf2(
+        final int number
+    ) {
+        return 1 < (number & -number);
+    }
+
     private BitFactoring() {}
-
-    /** Determines whether a number contains a factor of 2
-     * @param number the Number to check
-     * @return True if number is divisible by 2 */
-    public static boolean isProductOf2(
-            long number
-    ) {
-        return (number & -number) > 1;
-    }
-
-    /** Determines whether a number contains a factor of 2
-     * @param number the Number to check
-     * @return True if number is divisible by 2 */
-    public static boolean isProductOf2(
-            int number
-    ) {
-        return (number & -number) > 1;
-    }
 
 }
