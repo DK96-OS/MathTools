280e596ca3a09048c177e672b3cd21273e40fa45
DK96 <69859316+dk96-os@users.noreply.github.com>
2022-03-28 12:04:16 -0400

    Tests: * Update IntOperations tests - move exponent function test cases into independent file

--- numbers/src/test/java/mathtools/numbers/factors/IntOperationsTest.java
+++ numbers/src/test/java/mathtools/numbers/factors/IntOperationsTest.java
@@ -1,9 +1,7 @@
 package mathtools.numbers.factors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static mathtools.numbers.factors.IntOperations.exponent;
 import static mathtools.numbers.factors.IntOperations.tenShift;
-import static mathtools.numbers.structs.IntPairTest.assertPairEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -108,76 +106,6 @@ public final class IntOperationsTest {
 		// combination of x and exponent
 		assertEquals(MAX, tenShift(200, 8));
 		assertEquals(MAX, tenShift(200, 9));
-	}
-
-	@Test
-	void testExponent() {
-		assertPairEquals(1, 0, exponent(1, 2));
-		assertPairEquals(4, 0, exponent(2, 2));
-		assertPairEquals(16, 0, exponent(2, 4));
-		assertPairEquals(32, 0, exponent(2, 5));
-		assertPairEquals(256, 0, exponent(2, 8));
-		assertPairEquals(1024, 0, exponent(2, 10));
-		assertPairEquals(2048, 0, exponent(2, 11));
-	}
-
-	@Test
-	void testExponentZeroAndOneCases() {
-		assertPairEquals(1, 0, exponent(1, 5));
-		assertPairEquals(0, 0, exponent(0, 5));
-		//
-		assertPairEquals(1, 0, exponent(6, 0));
-		assertPairEquals(1, 0, exponent(-6, 0));
-		assertPairEquals(6, 0, exponent(6, 1));
-	}
-
-	@Test
-	void testExponentNegativeOneX() {
-		assertPairEquals(1, 0, exponent(-1, 0));
-		assertPairEquals(-1, 0, exponent(-1, 1));
-		assertPairEquals(1, 0, exponent(-1, 2));
-		assertPairEquals(-1, 0, exponent(-1, 3));
-	}
-
-	@Test
-	void testExponentOverflow() {
-		// This would overflow 16-bit short
-		assertPairEquals(65536, 0, exponent(256, 2));
-		// Overflow 32-bit int
-		// Should be 256 ^ 4 = 256 * 256 * 256 * 256 = 4294967296
-		// Instead, 256 ^ 3 = 16777216, with a remaining power of 1
-		assertPairEquals(16777216, 1, exponent(256, 4));
-		// Should be 256 ^ 5
-		// Instead, 256 ^ 3 = 16777216, with a remaining power of 2
-		assertPairEquals(16777216, 2, exponent(256, 5));
-	}
-
-	@Test
-	void testExponentOverflowTricky() {
-		assertPairEquals(66049, 0, exponent(257, 2));
-		assertPairEquals(16974593, 0, exponent(257, 3));
-		assertPairEquals(16974593, 1, exponent(257, 4));
-	}
-
-	@Test
-	void testExponentMaxValue() {
-		assertPairEquals(214748364, 3, exponent(214748364, 3));
-		assertPairEquals(2147483647, 4, exponent(Integer.MAX_VALUE, 4));
-	}
-
-	@Test
-	void testExponentNegativePower() {
-		for (int pwr = -1; pwr >= -10; pwr--)
-			assertPairEquals(2, pwr, exponent(2, pwr));
-	}
-
-	@Test
-	void testExponentNegativeX() {
-		assertPairEquals(1, 0, exponent(-2, 0));
-		assertPairEquals(-2, 0, exponent(-2, 1));
-		assertPairEquals(4, 0, exponent(-2, 2));
-		assertPairEquals(-8, 0, exponent(-2, 3));
-		assertPairEquals(16, 0, exponent(-2, 4));
 	}
 
 }
