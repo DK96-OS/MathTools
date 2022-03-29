522629138632fad54760305db3b43e96c4a9abc8
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2022-03-29 15:29:11 -0400

    Numbers Format Updates (#80)
    Format:
    * Convert Percentages to Java
    * Create NumberDeserializer
    * Create NumberSerializer
    * Create Rounding class
    * Update Rounding - make constructor private
    * Update StringPackager - deprecate methods, replace implementations with NumberSerializer or NumberDeserializer methods
    
    Tests:
    * Convert PercentagesTest to Java
    * Create NumberSerializerByteTest
    * Create NumberSerializerFloatTest
    * Create NumberSerializerShortTest
    * Create RoundingTest
    * Update NumberSerializerFloatTest - add test case, improve code style
    * Update Percentages.java - make constructor private
    * Update PercentagesTest - add test cases for coverage of the one arg strictPercent function
    * Update StringPackagerByteTest.java - fix infinite loop, use import to simplify
    * Update StringPackagerFloatTest.kt - improve code style
    * Update StringPackagerIntTest.kt - improve code style, improve test cases

---
+++ numbers/src/main/java/mathtools/numbers/format/NumberDeserializer.java
@@ -0,0 +1,69 @@
+package mathtools.numbers.format;
+
+import com.google.common.primitives.UnsignedInteger;
+
+import javax.annotation.Nonnull;
+
+import mathtools.numbers.structs.BytePair;
+
+/** Encode Numbers from Strings and characters
+ * @author DK96-OS : 2021 - 2022 */
+public final class NumberDeserializer {
+
+	/** Unpack 1st of 2 bytes from a character
+	 * @param character The character to extract from
+	 * @return The first Byte value that was stored in character */
+	public static byte getByte1(
+		final char character
+	) {
+		return (byte) (character >>> 8);
+	}
+
+	/** Unpack 2nd of 2 bytes from a character
+	 * @param character The character to extract from
+	 * @return The second Byte value that was stored in character */
+	public static byte getByte2(
+		final char character
+	) {
+		return (byte) character;
+	}
+
+	/** Extract 2 Byte from a Character
+	 * @param character The character to extract from
+	 * @return A BytePair containing the two bytes */
+	@Nonnull
+	public static BytePair getBytes(
+		final char character
+	) {
+		return new BytePair(
+			(byte) (character >>> 8),
+			(byte) character
+		);
+	}
+
+	/** Unpack a short integer from a character
+	 * @param character The character to extract from */
+	public static short getShort(
+		final char character
+	) {
+		return (short) character;
+	}
+
+	/** Unpack a 32-bit float from two 16-bit characters
+	 * @param char1 The first character containing half information
+	 * @param char2 The second character containing half information
+	 * @return The float that was serialized */
+	public static float getFloat(
+		final char char1,
+		final char char2
+	) {
+		return Float.intBitsToFloat(
+			UnsignedInteger.fromIntBits(char1)
+				.plus(UnsignedInteger.fromIntBits(char2 << 16))
+				.intValue()
+		);
+	}
+
+	private NumberDeserializer() {}
+
+}
---
+++ numbers/src/test/java/mathtools/numbers/format/NumberSerializerByteTest.java
@@ -0,0 +1,66 @@
+package mathtools.numbers.format;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static mathtools.numbers.format.NumberDeserializer.getByte1;
+import static mathtools.numbers.format.NumberDeserializer.getByte2;
+import static mathtools.numbers.format.NumberDeserializer.getBytes;
+import static mathtools.numbers.format.NumberSerializer.putBytes;
+
+import org.junit.jupiter.api.Test;
+
+import mathtools.numbers.structs.BytePair;
+import mathtools.numbers.structs.BytePairTest;
+
+/** Testing [NumberSerializer] and [NumberDeserializer] Byte methods
+ * @author DK96-OS : 2022 */
+public final class NumberSerializerByteTest {
+
+	@Test
+	public void testPositiveBytes() {
+		byte b1 = 0;
+		byte b2;
+		//
+		for (; 0 <= b1; b1 += 4) {
+			//
+			for (b2 = 0; 0 <= b2; b2 += 3) {
+				//
+				final char serialized = putBytes(b1, b2);
+				// Check individual byte unpacking
+				assertEquals(
+					b1, getByte1(serialized));
+				assertEquals(
+					b2, getByte2(serialized));
+				// Check pair of bytes unpacked
+				final BytePair deserializedPair = getBytes(serialized);
+				BytePairTest.assertPairEquals(
+					b1, b2, deserializedPair
+				);
+			}
+		}
+	}
+
+	@Test
+	public void testNegativeBytes() {
+		byte b1 = Byte.MIN_VALUE;
+		byte b2;
+		//
+		for (; 0 > b1; b1 += 4) {
+			//
+			for (b2 = Byte.MIN_VALUE; 0 > b2; b2 += 4) {
+				//
+				final char serialized = putBytes(b1, b2);
+				// Check individual byte unpacking
+				assertEquals(
+					b1, getByte1(serialized));
+				assertEquals(
+					b2, getByte2(serialized));
+				// Check pair function
+				final BytePair deserializedPair = getBytes(serialized);
+				BytePairTest.assertPairEquals(
+					b1, b2, deserializedPair
+				);
+			}
+		}
+	}
+
+}
