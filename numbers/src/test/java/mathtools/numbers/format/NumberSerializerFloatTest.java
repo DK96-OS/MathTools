15f591029f423c2cb87bc123a66f8c6a1f378906
DK96 <69859316+dk96-os@users.noreply.github.com>
2022-03-29 11:56:08 -0400

    Tests: * Update NumberSerializerFloatTest - add test case, improve code style * Create NumberSerializerShortTest

--- numbers/src/test/java/mathtools/numbers/format/NumberSerializerFloatTest.java
+++ numbers/src/test/java/mathtools/numbers/format/NumberSerializerFloatTest.java
@@ -3,18 +3,21 @@ package mathtools.numbers.format;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static mathtools.numbers.format.NumberDeserializer.getFloat;
 import static mathtools.numbers.format.NumberSerializer.putFloat;
+import static mathtools.numbers.format.NumberSerializer.putFloats;
 
 import org.junit.jupiter.api.Test;
 
-/** Testing [NumberSerializer] and [NumberDeserializer]
+/** Testing [NumberSerializer] and [NumberDeserializer] float methods
  * @author DK96-OS : 2022 */
 public final class NumberSerializerFloatTest {
 
+	static final float[] ARRAY = new float[0];
+
 	@Test
-	void testFloat() {
-		for (int e = 0; e < 128; ++e) {
+	void testPutFloat() {
+		for (int e = 0; 128 > e; ++e) {
 			final int exp = e << 23;
-			for (int m = 0; m < 23; ++m) {
+			for (int m = 0; 23 > m; ++m) {
 				final int mBit = 1 << m;
 				final float f = Float.intBitsToFloat(exp + mBit);
 				final String serial = putFloat(f);
@@ -25,6 +28,12 @@ public final class NumberSerializerFloatTest {
 					f, getFloat(serial.charAt(0), serial.charAt(1)));
 			}
 		}
+	}
+
+	@Test
+	public void testPutFloatsEmpty() {
+		assertEquals(
+			"", putFloats(ARRAY));
 	}
 
 }
