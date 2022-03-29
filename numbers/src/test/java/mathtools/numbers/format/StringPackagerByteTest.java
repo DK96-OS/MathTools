10d1d5ef8339ca3946b321d4bec9c101630ea063
DK96 <69859316+dk96-os@users.noreply.github.com>
2022-03-29 12:19:02 -0400

    Tests: * Update StringPackager tests

---
+++ numbers/src/test/java/mathtools/numbers/format/StringPackagerByteTest.java
@@ -0,0 +1,65 @@
+package mathtools.numbers.format;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import kotlin.Pair;
+
+/** Testing [StringPackager] methods
+ * @author DK96-OS : 2022 */
+public final class StringPackagerByteTest {
+
+	@Test
+	public void testPositiveBytes() {
+		byte b0 = 0;
+		byte b1;
+		//
+		for (; Byte.MAX_VALUE > b0; b0 += 4) {
+			//
+			for (b1 = 0; Byte.MAX_VALUE > b1; b1 += 4) {
+				//
+				final char packed = StringPackager.INSTANCE.packBytes(b0, b1);
+				// Check individual byte unpacking
+				assertEquals(
+					b0, StringPackager.INSTANCE.unpackByte0(packed));
+				assertEquals(
+					b1, StringPackager.INSTANCE.unpackByte1(packed));
+				// Check pair of bytes unpacked
+				final Pair<Byte, Byte> unpackedPair =
+					StringPackager.INSTANCE.unpackBytes(packed);
+				assertEquals(
+					b0, unpackedPair.getFirst());
+				assertEquals(
+					b1, unpackedPair.getSecond());
+			}
+		}
+	}
+
+	@Test
+	public void testNegativeBytes() {
+		byte b0 = Byte.MIN_VALUE;
+		byte b1;
+		//
+		for (; 0 > b0; b0 += 4) {
+			//
+			for (b1 = Byte.MIN_VALUE; 0 > b1; b1 += 4) {
+				//
+				final char packed = StringPackager.INSTANCE.packBytes(b0, b1);
+				// Check individual byte unpacking
+				assertEquals(
+					b0, StringPackager.INSTANCE.unpackByte0(packed));
+				assertEquals(
+					b1, StringPackager.INSTANCE.unpackByte1(packed));
+				// Check pair function
+				final Pair<Byte, Byte> unpackedPair =
+					StringPackager.INSTANCE.unpackBytes(packed);
+				assertEquals(
+					b0, unpackedPair.getFirst());
+				assertEquals(
+					b1, unpackedPair.getSecond());
+			}
+		}
+	}
+
+}
--- numbers/src/test/kotlin/mathtools/numbers/format/StringPackagerShortTest.kt
+++ numbers/src/test/kotlin/mathtools/numbers/format/StringPackagerShortTest.kt
@@ -1,11 +1,7 @@
 package mathtools.numbers.format
 
-import mathtools.numbers.format.StringPackager.packBytes
 import mathtools.numbers.format.StringPackager.packShort
 import mathtools.numbers.format.StringPackager.packUShort
-import mathtools.numbers.format.StringPackager.unpackByte0
-import mathtools.numbers.format.StringPackager.unpackByte1
-import mathtools.numbers.format.StringPackager.unpackBytes
 import mathtools.numbers.format.StringPackager.unpackShort
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNull
@@ -14,53 +10,7 @@ import org.junit.jupiter.api.Test
 
 /** Testing StringPackager functions of Integers
  * @author DK96-OS : 2022 */
-class StringPackagerIntTest {
-
-	@Test
-	fun testPositiveBytes() {
-		//
-		for (b0Int in 0 .. Byte.MAX_VALUE step 4) {
-			val b0 = b0Int.toByte()
-			//
-			for (b1Int in 0 .. Byte.MAX_VALUE step 4) {
-				val b1 = b1Int.toByte()
-				val packed = packBytes(b0, b1)
-				// Check individual byte unpacking
-				assertEquals(
-					b0, unpackByte0(packed))
-				assertEquals(
-					b1, unpackByte1(packed))
-				// Check pair of bytes unpacked
-				val unpackedPair = unpackBytes(packed)
-				assertEquals(
-					b0, unpackedPair.first)
-				assertEquals(
-					b1, unpackedPair.second)
-			}
-		}
-	}
-
-	@Test
-	fun testNegativeBytes() {
-		for (b0Int in Byte.MIN_VALUE until 0 step 4) {
-			val b0 = b0Int.toByte()
-			for (b1Int in Byte.MIN_VALUE until 0 step 4) {
-				val b1 = b1Int.toByte()
-				val packed = packBytes(b0, b1)
-				// Check individual byte unpacking
-				assertEquals(
-					b0, unpackByte0(packed))
-				assertEquals(
-					b1, unpackByte1(packed))
-				// Check pair function
-				val unpackedPair = unpackBytes(packed)
-				assertEquals(
-					b0, unpackedPair.first)
-				assertEquals(
-					b1, unpackedPair.second)
-			}
-		}
-	}
+class StringPackagerShortTest {
 
 	@Test
 	fun testPositiveShorts() {
