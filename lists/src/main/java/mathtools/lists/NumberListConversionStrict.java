d23afff604dce6af207ddd85d394e0af2df28f50
DK96 <69859316+dk96-os@users.noreply.github.com>
2022-03-30 13:02:09 -0400

    Lists: * Update NumberListConversionStrict - improve code style, add Integer and Long methods

--- lists/src/main/java/mathtools/lists/NumberListConversionStrict.java
+++ lists/src/main/java/mathtools/lists/NumberListConversionStrict.java
@@ -14,49 +14,105 @@ public final class NumberListConversionStrict {
 
     private NumberListConversionStrict() {}
 
-    /** Attempts to convert to a list of bytes
+    /** Attempts to convert to a list of Byte
      * @param list The input list to convert
      * @param ignoreInvalid Whether to ignore errors, or throw exception
      * @return A list of Bytes that were converted exactly */
     @Nonnull
     public static List<Byte> toByteStrict(
-            @Nonnull final List<Number> list,
-            final boolean ignoreInvalid
+        @Nonnull final List<Number> list,
+        final boolean ignoreInvalid
     ) throws NumberFormatException {
-        if (list.isEmpty())
-            return Collections.emptyList();
+        // Check for empty list
+        if (list.isEmpty()) return Collections.emptyList();
+        // Allocate new List
         final ArrayList<Byte> newList = new ArrayList<>(list.size());
-        for (Number number : list) {
-            final byte b = number.byteValue();
-            if (isEquivalent(number, b))
-                newList.add(b);
+        for (final Number number : list) {
+            final byte value = number.byteValue();
+            // Determine if the conversion is lossless
+            if (isEquivalent(number, value))
+                newList.add(value);
             else if (!ignoreInvalid)
                 throw new NumberFormatException(
-                        "Cannot convert " + number + " to byte"
+                    "Cannot convert " + number + " to byte"
                 );
         }
         return newList;
     }
 
-    /** Attempts to convert to a list of shorts
+    /** Attempts to convert to a list of Short
      * @param list The input list to convert
      * @param ignoreInvalid Whether to ignore errors, or throw exception
      * @return A list of Short that were converted exactly */
     @Nonnull
     public static List<Short> toShortStrict(
-            @Nonnull final List<Number> list,
-            final boolean ignoreInvalid
+        @Nonnull final List<Number> list,
+        final boolean ignoreInvalid
     ) throws NumberFormatException {
-        if (list.isEmpty())
-            return Collections.emptyList();
+        // Check for empty list
+        if (list.isEmpty()) return Collections.emptyList();
+        // Allocate new List
         final ArrayList<Short> newList = new ArrayList<>(list.size());
-        for (Number number : list) {
-            final short b = number.shortValue();
-            if (isEquivalent(number, b))
-                newList.add(b);
+        for (final Number number : list) {
+            final short value = number.shortValue();
+            // Determine if the conversion is lossless
+            if (isEquivalent(number, value))
+                newList.add(value);
             else if (!ignoreInvalid)
                 throw new NumberFormatException(
-                        "Cannot convert " + number + " to short"
+                    "Cannot convert " + number + " to short"
+                );
+        }
+        return newList;
+    }
+
+    /** Attempts to convert to a list of Integer
+     * @param list The input list to convert
+     * @param ignoreInvalid Whether to ignore errors, or throw exception
+     * @return A list of Integer that were converted exactly */
+    @Nonnull
+    public static List<Integer> toIntStrict(
+        @Nonnull final List<Number> list,
+        final boolean ignoreInvalid
+    ) throws NumberFormatException {
+        // Check for empty list
+        if (list.isEmpty()) return Collections.emptyList();
+        // Allocate new List
+        final ArrayList<Integer> newList = new ArrayList<>(list.size());
+        for (final Number number : list) {
+            final int value = number.intValue();
+            // Determine if the conversion is lossless
+            if (isEquivalent(number, value))
+                newList.add(value);
+            else if (!ignoreInvalid)
+                throw new NumberFormatException(
+                    "Cannot convert " + number + " to integer"
+                );
+        }
+        return newList;
+    }
+
+    /** Attempts to convert to a list of Long
+     * @param list The input list to convert
+     * @param ignoreInvalid Whether to ignore errors, or throw exception
+     * @return A list of Long that were converted exactly */
+    @Nonnull
+    public static List<Long> toLongStrict(
+        @Nonnull final List<Number> list,
+        final boolean ignoreInvalid
+    ) throws NumberFormatException {
+        // Check for empty list
+        if (list.isEmpty()) return Collections.emptyList();
+        // Allocate new List
+        final ArrayList<Long> newList = new ArrayList<>(list.size());
+        for (final Number number : list) {
+            final long value = number.intValue();
+            // Determine if the conversion is lossless
+            if (isEquivalent(number, value))
+                newList.add(value);
+            else if (!ignoreInvalid)
+                throw new NumberFormatException(
+                    "Cannot convert " + number + " to long"
                 );
         }
         return newList;
