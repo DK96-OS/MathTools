a821e2f85b8363087235b6fe7af0655d3930684d
DK96 <69859316+dk96-os@users.noreply.github.com>
2022-04-05 09:41:46 -0400

    Lists: * Update ByteArrayExt - update code style, add findTargetValueN method

--- lists/src/main/java/mathtools/lists/arrays/ByteArrayExt.java
+++ lists/src/main/java/mathtools/lists/arrays/ByteArrayExt.java
@@ -1,5 +1,7 @@
 package mathtools.lists.arrays;
 
+import com.google.common.primitives.Bytes;
+
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.jetbrains.annotations.NotNull;
 
@@ -14,13 +16,11 @@ import javax.annotation.Nonnull;
  * @author DK96-OS : 2022 */
 public final class ByteArrayExt {
 
-    private ByteArrayExt() {}
-
     /** Compute the sum of all elements in the array
      * @param array The array of elements
      * @return A Long value containing the sum of the elements */
     public static long sum(
-            @NotNull byte[] array
+        @NotNull final byte[] array
     ) {
         long sum = 0;
         for (byte b : array) sum += b;
@@ -31,7 +31,7 @@ public final class ByteArrayExt {
      *  Uses java.util.Arrays.fill()
      * @param array The array to reset to all zero */
     public static void clear(
-            @NotNull byte[] array
+        @NotNull final byte[] array
     ) {
         Arrays.fill(array, (byte) 0);
     }
@@ -39,9 +39,9 @@ public final class ByteArrayExt {
     /** Create a List from the values in an array */
     @NonNull
     public static List<Byte> toList(
-            @Nonnull final byte[] array
+        @Nonnull final byte[] array
     ) {
-        if (array.length < 5) switch (array.length) {
+        if (5 > array.length) switch (array.length) {
             case 4: return List.of(array[0], array[1], array[2], array[3]);
             case 3: return List.of(array[0], array[1], array[2]);
             case 2: return List.of(array[0], array[1]);
@@ -55,11 +55,43 @@ public final class ByteArrayExt {
 
     /** Determines whether all Integer values in this array are non-zero */
     public static boolean allNonZero(
-            @Nonnull final byte[] array
+        @Nonnull final byte[] array
     ) {
         for (byte j : array)
             if (0 == j) return false;
         return true;
     }
+
+    /** Search the array from start to end, for a specific repeating value
+     * @param array The array to search in
+     * @param target The target values to search for
+     * @param counter The number of target values to find
+     * @return The index of the n-th value, or -1 if  */
+    public static int findTargetValueN(
+        @Nonnull final byte[] array,
+        final byte target,
+        int counter
+    ) {
+        final int firstIndex = Bytes.indexOf(array, target);
+        //
+        if (-1 == firstIndex) return -1;
+        // If only 1 is required
+        if (1 == counter--) return firstIndex;
+        // Continue the search
+        int searchIndex = firstIndex + 1;
+        for (;
+             searchIndex < array.length;
+             ++searchIndex
+        ) {
+            if (target == array[searchIndex]) {
+                // When counter is done, return the index
+                if (0 == --counter) return searchIndex;
+            }
+        }
+        // Done reading array, not enough target values found
+        return -1;
+    }
+
+    private ByteArrayExt() {}
 
 }
