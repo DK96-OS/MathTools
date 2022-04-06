d9e00bf0a42315b6ae98639b96cf3e5adc6237df
DK96 <69859316+dk96-os@users.noreply.github.com>
2022-04-06 16:22:56 -0400

    Tests: * Update LinearShortElementTest - add test cases, improve code style and quality by using static values

--- generators/src/test/java/mathtools/generators/elements/shorts/LinearShortElementTest.java
+++ generators/src/test/java/mathtools/generators/elements/shorts/LinearShortElementTest.java
@@ -8,28 +8,54 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
+import mathtools.generators.RandomProvider;
+
 /** Testing [LinearShortElement] class
  * @author DK96-OS : 2022 */
 public final class LinearShortElementTest {
 
 	private LinearShortElement mElement;
 
+	private static final short initialLength = 6;
+	private static final short initialRate = 1;
+
 	@BeforeEach
 	public void testSetup() {
-		mElement = new LinearShortElement((short) 6, (short) 3);
+		mElement = new LinearShortElement(
+			initialLength,
+			initialRate
+		);
 	}
 
 	@Test
 	public void testConstructorInvalidArgs() {
+		// Length and rate must be positive, non-zero
 		assertThrows(IllegalArgumentException.class,
-			() -> new LinearShortElement((short) 0, (short) 3));
+			() -> new LinearShortElement((short) 0, initialRate));
 		assertThrows(IllegalArgumentException.class,
-			() -> new LinearShortElement((short) -1, (short) 3));
-		//
+			() -> new LinearShortElement(initialLength, (short) 0));
+		// Length and rate are limited to 200
 		assertThrows(IllegalArgumentException.class,
-			() -> new LinearShortElement((short) 5, (short) 0));
+			() -> new LinearShortElement((short) 201, initialRate));
 		assertThrows(IllegalArgumentException.class,
-			() -> new LinearShortElement((short) 5, (short) -1));
+			() -> new LinearShortElement(initialLength, (short) 201));
+	}
+
+	@Test
+	public void testConstructorRandomInvalidArgs() {
+		final Random rng = RandomProvider.fixedValue(1);
+		// Length and rate must be positive, non-zero
+		assertThrows(IllegalArgumentException.class,
+			() -> new LinearShortElement((short) 0, initialRate, rng));
+		assertThrows(IllegalArgumentException.class,
+			() -> new LinearShortElement(initialLength, (short) 0, rng));
+		// Length and rate are limited to 200
+		assertThrows(IllegalArgumentException.class,
+			() -> new LinearShortElement((short) 201, initialRate, rng));
+		assertThrows(IllegalArgumentException.class,
+			() -> new LinearShortElement(initialLength, (short) 201, rng));
 	}
 
 	@Test
@@ -37,11 +63,14 @@ public final class LinearShortElementTest {
 		assertEquals(
 			6, mElement.getLength());
 		assertEquals(
-			3, mElement.getRate());
+			1, mElement.getRate());
 	}
 
 	@Test
 	public void testLengthSetter() {
+		assertFalse(
+			mElement.setLength(initialLength));
+		// Set to different value
 		assertTrue(
 			mElement.setLength((short) 80));
 		assertEquals(
@@ -57,25 +86,27 @@ public final class LinearShortElementTest {
 		assertFalse(
 			mElement.setLength((short) 0));
 		assertFalse(
-			mElement.setLength((short) -1));
-		assertFalse(
 			mElement.setLength((short) 181));
+		// Value is the same as initial
 		assertEquals(
-			6, mElement.getLength());
+			initialLength, mElement.getLength());
 	}
 
 	@Test
 	public void testRateSetter() {
+		assertFalse(
+			mElement.setRate(initialRate)); // Same as initial value
+		// Set to different value succeeds
 		assertTrue(
 			mElement.setRate((short) 50));
 		assertEquals(
 			50, mElement.getRate());
+	}
+
+	@Test
+	public void testRateSetterMaximumValue() {
 		assertTrue(
-			mElement.setRate((short) 1));
-		assertEquals(
-			1, mElement.getRate());
-		assertTrue(
-			mElement.setRate((short) 200));
+			mElement.setRate((short) 200)); // Maximum accepted value
 		assertEquals(
 			200, mElement.getRate());
 	}
@@ -85,11 +116,9 @@ public final class LinearShortElementTest {
 		assertFalse(
 			mElement.setRate((short) 0));
 		assertFalse(
-			mElement.setRate((short) -1));
-		assertFalse(
 			mElement.setRate((short) 201));
 		assertEquals(
-			3, mElement.getRate());
+			initialRate, mElement.getRate());
 	}
 
 }
