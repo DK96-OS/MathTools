42ebfb12774945ff1ce2db15c24b3bbdfb683a97
DK96 <69859316+dk96-os@users.noreply.github.com>
2022-04-28 16:04:12 -0400

    Tests: * Update PrimeCachePerformanceTest * Update PrimeValidationPerformanceTest

---
+++ numbers/src/test/java/mathtools/numbers/primes/cache/PrimeCachePerformanceTest.java
@@ -0,0 +1,84 @@
+package mathtools.numbers.primes.cache;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import mathtools.numbers.primes.BytePrimeCache;
+import mathtools.numbers.primes.PrimeCacheInterface;
+import mathtools.numbers.structs.TimeMeasurement;
+import mathtools.statistics.Statistics;
+
+/** Performance Testing for Prime Number Caches
+ * @author DK96-OS : 2022 */
+public final class PrimeCachePerformanceTest {
+
+	/** The number of data points per test */
+	private static final int N_DATA_POINTS = 10_000;
+
+	/** The Prime Cache to use during tests */
+	private PrimeCacheInterface cache;
+
+	/** The data obtained from Time Measurements */
+	private ArrayList<Long> timeData;
+
+	@BeforeEach
+	public void testSetup() {
+		cache = new BytePrimeCache();
+		timeData = new ArrayList<>(N_DATA_POINTS);
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {
+		0, 4, 9, 12, 15, 16, 16, 16, 17, 17, 18, 20, 22, 24, 24, 25,
+		26, 28, 30, 32, 34, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 53
+	})
+	public void testTargetedAccess(
+		final int target
+	) {
+		Integer foundPrime = null;
+		final TimeMeasurement measurement = new TimeMeasurement();
+		for (
+			int i = 0;
+			N_DATA_POINTS > i;
+			++i
+		) {
+			measurement.reset();
+			foundPrime = cache.getPrime(target);
+			timeData.add(measurement.measure());
+			cache.clear();
+		}
+		printTestResults(
+			"TestTargetedAccess",
+			String.format(
+				"%d, %d", target, foundPrime
+			),
+			timeData
+		);
+	}
+
+	/** Process and print a statistical summary of the data */
+	public static void printTestResults(
+		final String testName,
+		final String params,
+		final ArrayList<Long> dataList
+	) {
+		final double mean = Statistics.INSTANCE.calculateMean(dataList);
+		final double sDev = Statistics.INSTANCE.calculateSDev(dataList, mean);
+		final Long min = Collections.min(dataList);
+		//
+		System.out.printf(
+			"%s(%s): u=%f, o-=%f, min=%d\n",
+			testName,
+			params,
+			mean,
+			sDev,
+			min
+		);
+	}
+
+}
--- numbers/src/test/java/mathtools/numbers/primes/validate/PrimeValidationPerformanceTest.java
+++ numbers/src/test/java/mathtools/numbers/primes/validate/PrimeValidationPerformanceTest.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 
 import mathtools.numbers.primes.BytePrimeCache;
 import mathtools.numbers.primes.PrimeCacheInterface;
-import mathtools.numbers.primes.PrimeCachePerformanceTest;
+import mathtools.numbers.primes.cache.PrimeCachePerformanceTest;
 import mathtools.numbers.structs.TimeMeasurement;
 
 /** Performance Measurements of PrimeValidation methods
@@ -16,7 +16,7 @@ import mathtools.numbers.structs.TimeMeasurement;
 public final class PrimeValidationPerformanceTest {
 
 	/** The number of data points per test */
-	private static final int nDataPoints = 10_000;
+	private static final int N_DATA_POINTS = 10_000;
 
 	/** The Prime Cache to use during tests */
 	private PrimeCacheInterface cache;
@@ -27,14 +27,14 @@ public final class PrimeValidationPerformanceTest {
 	@BeforeEach
 	public void testSetup() {
 		cache = new BytePrimeCache();
-		timeData = new ArrayList<>(nDataPoints);
+		timeData = new ArrayList<>(N_DATA_POINTS);
 	}
 
 	@ParameterizedTest
 	@ValueSource(ints = {
-		9, 9, 15, 15, 21, 25, 25, 27, 29, 33, 35,
-		55, 55, 71, 71, 75, 75, 75, 77, 77, 91, 93, 95, 97, 183, 185,
-		187, 189, 191, 213, 215, 217, 219, 243, 245, 247, 249, 251,
+		9, 9, 15, 15, 21, 25, 25, 27, 29, 33, 35, 55, 55, 71, 71,
+		75, 75, 75, 77, 77, 91, 93, 95, 97, 183, 185, 187, 189,
+		191, 213, 215, 217, 219, 243, 245, 247, 249, 251
 	})
 	public void testFindPrime(
 		final int startNumber
@@ -44,7 +44,7 @@ public final class PrimeValidationPerformanceTest {
 		Integer result = null;
 		//
 		for (int counter = 0;
-		     nDataPoints > counter;
+		     N_DATA_POINTS > counter;
 		     ++counter
 		) {
 			measurement.reset();
@@ -53,9 +53,40 @@ public final class PrimeValidationPerformanceTest {
 			);
 			timeData.add(measurement.measure());
 		}
-		PrimeCachePerformanceTest.Companion.printTestResults(
+		PrimeCachePerformanceTest.printTestResults(
 			"FindPrime",
 			String.format("%d -> %d", startNumber, result),
+			timeData
+		);
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {
+		11, 11, 15, 15, 25, 25, 29, 29, 71, 73, 75, 109,
+		141, 143, 143, 145, 145, 147, 197, 199, 199, 201,
+		219, 221, 223, 241, 243, 245, 249, 251, 253, 255
+	})
+	public void testIsPrime(
+		final int target
+	) {
+		Boolean wasPrime = null;
+		final TimeMeasurement measurement = new TimeMeasurement();
+		for (
+			int i = 0;
+			N_DATA_POINTS > i;
+			++i
+		) {
+			measurement.reset();
+			wasPrime = PrimeValidation.isPrime(
+				target, cache
+			);
+			timeData.add(measurement.measure());
+		}
+		PrimeCachePerformanceTest.printTestResults(
+			"TestIsPrime",
+			String.format(
+				"%d, %s", target, wasPrime
+			),
 			timeData
 		);
 	}
