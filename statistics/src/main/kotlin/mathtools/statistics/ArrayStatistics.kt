7f2cb530271288f0adfc1a156701db1677bc301a
DK96 <69859316+dk96-os@users.noreply.github.com>
2022-04-29 12:07:46 -0400

    Documentation: * Update ArrayStatistics.kt - add docs

--- statistics/src/main/kotlin/mathtools/statistics/ArrayStatistics.kt
+++ statistics/src/main/kotlin/mathtools/statistics/ArrayStatistics.kt
@@ -4,7 +4,11 @@ import kotlin.math.sqrt
 
 /* The Standard Deviation functions */
 
-/** Calculate the standard deviation of an 8 bit Byte Array */
+/** Calculate the standard deviation of an 8 bit Byte Array
+ * @param array The Array containing the data
+ * @param mean The Mean (average) value of the data in the Array
+ * @return A Double representing the Standard Deviation
+ */
 fun calculateSDev(
     array: ByteArray,
     mean: Double,
@@ -18,7 +22,11 @@ fun calculateSDev(
 	return sqrt(varianceSum / (array.size - 1))
 }
 
-/** Calculate the standard deviation of a 16 bit Short Array */
+/** Calculate the standard deviation of a 16 bit Short Array
+ * @param array The Array containing the data
+ * @param mean The Mean (average) value of the data in the Array
+ * @return A Double representing the Standard Deviation
+ */
 fun calculateSDev(
     array: ShortArray,
     mean: Double
@@ -32,7 +40,11 @@ fun calculateSDev(
 	return sqrt(varianceSum / (array.size - 1))
 }
 
-/** Calculate the standard deviation of a 32 bit Int Array */
+/** Calculate the standard deviation of a 32 bit Integer Array
+ * @param array The Array containing the data
+ * @param mean The Mean (average) value of the data in the Array
+ * @return A Double representing the Standard Deviation
+ */
 fun calculateSDev(
     array: IntArray,
     mean: Double
@@ -46,7 +58,11 @@ fun calculateSDev(
 	return sqrt(varianceSum / (array.size - 1))
 }
 
-/** Calculate the standard deviation of a 64 bit Long Array */
+/** Calculate the standard deviation of a 64 bit Long Array
+ * @param array The Array containing the data
+ * @param mean The Mean (average) value of the data in the Array
+ * @return A Double representing the Standard Deviation
+ */
 fun calculateSDev(
     array: LongArray,
     mean: Double
@@ -60,7 +76,11 @@ fun calculateSDev(
 	return sqrt(varianceSum / (array.size - 1))
 }
 
-/** Calculate the standard deviation of a 32 bit Float Array */
+/** Calculate the standard deviation of a 32 bit Float Array
+ * @param array The Array containing the data
+ * @param mean The Mean (average) value of the data in the Array
+ * @return A Double representing the Standard Deviation
+ */
 fun calculateSDev(
     array: FloatArray,
     mean: Double
@@ -74,7 +94,11 @@ fun calculateSDev(
 	return sqrt(varianceSum / (array.size - 1))
 }
 
-/** Calculate the standard deviation of a 64 bit Double Array */
+/** Calculate the standard deviation of a 64 bit Double Array
+ * @param array The Array containing the data
+ * @param mean The Mean (average) value of the data in the Array
+ * @return A Double representing the Standard Deviation
+ */
 fun calculateSDev(
     array: DoubleArray,
     mean: Double
