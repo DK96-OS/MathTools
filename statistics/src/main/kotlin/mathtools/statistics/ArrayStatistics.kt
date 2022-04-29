038a464448bf714f591de80f9b38ba7d98f190e3
DK96 <69859316+dk96-os@users.noreply.github.com>
2022-04-29 12:05:05 -0400

    Statistics: * Update ArrayStatistics.kt - remove default parameter from calculateSDev methods

--- statistics/src/main/kotlin/mathtools/statistics/ArrayStatistics.kt
+++ statistics/src/main/kotlin/mathtools/statistics/ArrayStatistics.kt
@@ -7,7 +7,7 @@ import kotlin.math.sqrt
 /** Calculate the standard deviation of an 8 bit Byte Array */
 fun calculateSDev(
     array: ByteArray,
-    mean: Double = ArrayStatistics.calculateMean(array)
+    mean: Double,
 ) : Double {
     if (array.size < 3) return 0.0
     var varianceSum = 0.0
@@ -21,7 +21,7 @@ fun calculateSDev(
 /** Calculate the standard deviation of a 16 bit Short Array */
 fun calculateSDev(
     array: ShortArray,
-    mean: Double = ArrayStatistics.calculateMean(array)
+    mean: Double
 ) : Double {
     if (array.size < 3) return 0.0
     var varianceSum = 0.0
@@ -35,7 +35,7 @@ fun calculateSDev(
 /** Calculate the standard deviation of a 32 bit Int Array */
 fun calculateSDev(
     array: IntArray,
-    mean: Double = ArrayStatistics.calculateMean(array)
+    mean: Double
 ) : Double {
     if (array.size < 3) return 0.0
     var varianceSum = 0.0
@@ -49,7 +49,7 @@ fun calculateSDev(
 /** Calculate the standard deviation of a 64 bit Long Array */
 fun calculateSDev(
     array: LongArray,
-    mean: Double = ArrayStatistics.calculateMean(array)
+    mean: Double
 ) : Double {
     if (array.size < 3) return 0.0
     var varianceSum = 0.0
@@ -63,7 +63,7 @@ fun calculateSDev(
 /** Calculate the standard deviation of a 32 bit Float Array */
 fun calculateSDev(
     array: FloatArray,
-    mean: Double = ArrayStatistics.calculateMean(array)
+    mean: Double
 ) : Double {
     if (array.size < 3) return 0.0
     var varianceSum = 0.0
@@ -77,7 +77,7 @@ fun calculateSDev(
 /** Calculate the standard deviation of a 64 bit Double Array */
 fun calculateSDev(
     array: DoubleArray,
-    mean: Double = ArrayStatistics.calculateMean(array)
+    mean: Double
 ) : Double {
     if (array.size < 3) return 0.0
     var varianceSum = 0.0
