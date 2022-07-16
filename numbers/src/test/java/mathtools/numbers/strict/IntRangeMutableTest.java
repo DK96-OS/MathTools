5d17641b19d52598010bab85943919b584585661
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2022-07-16 17:41:40 -0400

    Test: * Update IntRangeMutableTest - add test cases, add test setup

--- numbers/src/test/java/mathtools/numbers/strict/IntRangeMutableTest.java
+++ numbers/src/test/java/mathtools/numbers/strict/IntRangeMutableTest.java
@@ -1,7 +1,10 @@
 package mathtools.numbers.strict;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /** Testing IntRange Mutable.
@@ -9,9 +12,32 @@ import org.junit.jupiter.api.Test;
  */
 public final class IntRangeMutableTest {
 
+	private static final int initialStart = 1;
+
+	private static final int initialLast = 3200;
+
+	private IntRangeMutable mRange;
+
+	@BeforeEach
+	public void testSetup() {
+		mRange = new IntRangeMutable(
+			initialStart, initialLast
+		);
+	}
+
+	@Test
+	public void testInitialCondition() {
+		assertEquals(
+			initialStart, mRange.getStartValue()
+		);
+		assertEquals(
+			initialLast, mRange.getLastValue()
+		);
+	}
+
 	@Test
 	public void testSimpleValues() {
-		IntRange range = new IntRangeMutable(0, 1);
+		final IntRange range = new IntRangeMutable(0, 1);
 		assertEquals(
 			0, range.getStartValue()
 		);
@@ -23,7 +49,9 @@ public final class IntRangeMutableTest {
 	@Test
 	public void testSingleValue() {
 		final int value = 1;
-		IntRange range = new IntRangeMutable(value, value);
+		final IntRange range = new IntRangeMutable(
+			value, value
+		);
 		assertEquals(
 			value, range.getStartValue()
 		);
@@ -34,7 +62,7 @@ public final class IntRangeMutableTest {
 
 	@Test
 	public void testReversedRange() {
-		IntRange range = new IntRangeMutable(
+		final IntRange range = new IntRangeMutable(
 			Integer.MAX_VALUE, Integer.MIN_VALUE
 		);
 		assertEquals(
@@ -43,6 +71,81 @@ public final class IntRangeMutableTest {
 		assertEquals(
 			Integer.MAX_VALUE, range.getLastValue()
 		);
+	}
+
+	@Test
+	public void testSetStartAboveCurrent() {
+		final int nextStart = 3;
+		assertTrue(
+			mRange.setStart(nextStart)
+		);
+		assertEquals(
+			nextStart, mRange.getStartValue()
+		);
+	}
+
+	@Test
+	public void testSetStartBelowCurrent() {
+		final int nextStart = -1;
+		assertTrue(
+			mRange.setStart(nextStart)
+		);
+		assertEquals(
+			nextStart, mRange.getStartValue()
+		);
+	}
+
+	@Test
+	public void testSetStartAboveLast() {
+		final int nextStart = initialLast + 30000;
+		assertFalse(
+			mRange.setStart(nextStart)
+		);
+		assertEquals(
+			initialStart, mRange.getStartValue()
+		);
+	}
+
+	@Test
+	public void testSetLastAboveCurrent() {
+		final int nextLast = initialLast + 300;
+		assertTrue(
+			mRange.setLast(nextLast)
+		);
+		assertEquals(
+			nextLast, mRange.getLastValue()
+		);
+	}
+
+	@Test
+	public void testSetLastBelowCurrent() {
+		final int nextLast = initialLast - 10;
+		assertTrue(
+			mRange.setLast(nextLast)
+		);
+		assertEquals(
+			nextLast, mRange.getLastValue()
+		);
+	}
+
+	@Test
+	public void testSetLastBelowStart() {
+		final int nextLast = initialStart - 1;
+		assertFalse(
+			mRange.setLast(nextLast)
+		);
+		assertEquals(
+			initialLast, mRange.getLastValue()
+		);
+	}
+
+	@Test
+	public void testExpandRangeTo() {
+
+	}
+
+	@Test
+	public void test() {
 	}
 
 }
