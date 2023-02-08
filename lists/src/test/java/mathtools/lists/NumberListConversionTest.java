f36ec1981a4a7b88b73d97aa34ea682f88f5b45f
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2023-02-07 19:58:15 -0500

    Tests: * Update NumberListConversionTest - add test cases

--- lists/src/test/java/mathtools/lists/NumberListConversionTest.java
+++ lists/src/test/java/mathtools/lists/NumberListConversionTest.java
@@ -11,29 +11,105 @@ import java.util.List;
  * @author DK96-OS : 2022 */
 public final class NumberListConversionTest {
 
-    /**  */
     @Test
-    void testShortToByte() {
-        ArrayList<Number> shortList = new ArrayList<>();
-        shortList.add((short) 100);
-        shortList.add((short) 200);
-        shortList.add((short) 400);
-        List<Byte> result = NumberListConversion.toByte(shortList);
-        assertEquals((byte) 100, result.get(0));
-        assertEquals((byte) (200 - 256), result.get(1));
-        assertEquals((byte) (400 - 256), result.get(2));
+    public void testToDouble() {
+        List<Number> input = NumberListTestResources.intList;
+        List<Double> result = NumberListConversion.toDouble(input);
+        assertEquals(
+            input.size(), result.size()
+        );
+        for (int index = 0;
+            index < input.size();
+            index++
+        ) assertEquals(
+            input.get(index).doubleValue(),
+            result.get(index)
+        );
     }
 
     @Test
-    void testFloatToByte() {
-        ArrayList<Number> floatList = new ArrayList<>();
-        floatList.add(0.23f);
-        floatList.add(10.2f);
-        floatList.add(200.3f);
-        List<Byte> result = NumberListConversion.toByte(floatList);
-        assertEquals((byte) 0, result.get(0));
-        assertEquals((byte) 10, result.get(1));
-        assertEquals((byte) (200 - 256), result.get(2));
+    public void testToFloat() {
+        List<Number> input = NumberListTestResources.intList;
+        List<Float> result = NumberListConversion.toFloat(input);
+        assertEquals(
+            input.size(), result.size()
+        );
+        for (int index = 0;
+            index < input.size();
+            index++
+        ) assertEquals(
+            input.get(index).floatValue(),
+            result.get(index)
+        );
+    }
+
+    @Test
+    public void testToLong() {
+        List<Number> input = NumberListTestResources.intList;
+        List<Long> result = NumberListConversion.toLong(input);
+        assertEquals(
+            input.size(), result.size()
+        );
+        for (int index = 0;
+            index < input.size();
+            index++
+        ) assertEquals(
+            input.get(index).longValue(),
+            result.get(index)
+        );
+    }
+
+    @Test
+    public void testToInt() {
+        List<Number> input = NumberListTestResources.shortList;
+        List<Integer> result = NumberListConversion.toInt(input);
+        assertEquals(
+            input.size(), result.size()
+        );
+        for (int index = 0;
+             index < input.size();
+             index++
+        ) assertEquals(
+            input.get(index).intValue(),
+            result.get(index)
+        );
+    }
+
+    /**  */
+    @Test
+    void testToByte_InputShorts() {
+        ArrayList<Number> input = new ArrayList<>();
+        input.add((short) 100);
+        input.add((short) 200);
+        input.add((short) 400);
+        List<Byte> result = NumberListConversion.toByte(input);
+        assertEquals(
+            (byte) 100, result.get(0)
+        );
+        assertEquals(
+            (byte) (200 - 256), result.get(1)
+        );
+        assertEquals(
+            (byte) (400 - 256), result.get(2)
+        );
+    }
+
+    @Test
+    void testToByte_InputFloats() {
+        ArrayList<Number> input = new ArrayList<>();
+        input.add(0.23f);
+        input.add(10.2f);
+        input.add(200.3f);
+        List<Byte> result = NumberListConversion.toByte(input);
+        assertEquals(
+            (byte) 0, result.get(0)
+        );
+        assertEquals(
+            (byte) 10, result.get(1)
+        );
+        assertEquals(
+            (byte) (200 - 256), result.get(2)
+        );
     }
 
 }
