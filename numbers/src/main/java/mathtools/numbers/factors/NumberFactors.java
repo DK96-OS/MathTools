00eb3df89e85867d04759adf9e861093ce40cb3c
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2023-03-15 12:03:39 -0400

    Numbers Factors: * Update NumberFactors - update docs, improve code style

--- numbers/src/main/java/mathtools/numbers/factors/NumberFactors.java
+++ numbers/src/main/java/mathtools/numbers/factors/NumberFactors.java
@@ -1,26 +1,30 @@
 package mathtools.numbers.factors;
 
-/** Functions that Factor Numbers
- * @author DK96-OS : 2022 */
+/** Functions that Factor Numbers.
+ * @author DK96-OS : 2022 - 2023
+ */
 public final class NumberFactors {
 
-	/** Determines whether a number contains a factor of 5
-	 * @param number the Number to check
-	 * @return True if the number is divisible by 5 */
+	/** Determines whether a number contains a factor of 5.
+	 * @param number the Number to check.
+	 * @return True if the number is divisible by 5.
+	 */
 	public static boolean isProductOf5(
 		final long number
 	) {
 		final String nStr = Long.toString(number);
 		final int length = nStr.length() - 1;
 		final int d = Character.digit(nStr.charAt(length), 10);
-		return 5 == d || 0 == d && 0 < length;
+		return 5 == d
+			|| 0 == d && 0 < length;
 	}
 
 	/** Count the power of a factor in a composite number.
 	 *  Ignores negative signs.
-	 * @param factor The factor to check for
-	 * @param composite The number to check in
-	 * @return The power of factor in the composite */
+	 * @param factor The factor to check for.
+	 * @param composite The number to check in.
+	 * @return The power of factor in the composite.
+	 */
 	public static int countFactor(
 		final int factor,
 		final long composite
