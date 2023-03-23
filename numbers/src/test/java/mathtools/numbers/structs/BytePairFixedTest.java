bfbbccac7c0a68e0e875a658cbd8e708feb2ef71
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2023-03-23 14:23:00 -0400

    Tests: * Update BytePairTest - add test cases * Update BytePairFixedTest - add test cases

--- numbers/src/test/java/mathtools/numbers/structs/BytePairFixedTest.java
+++ numbers/src/test/java/mathtools/numbers/structs/BytePairFixedTest.java
@@ -1,7 +1,9 @@
 package mathtools.numbers.structs;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /** Testing [BytePairFixed] class.
@@ -12,15 +14,66 @@ public final class BytePairFixedTest {
 	private final byte number1 = 1;
 	private final byte number2 = 2;
 
+	private BytePairFixed mPair;
+
+	@BeforeEach
+	public void testSetup() {
+		mPair = new BytePairFixed(number1, number2);
+	}
+
 	@Test
-	public void testConstructor() {
-		final BytePairFixed pair = new BytePairFixed(number1, number2);
+	public void testInitialCondition() {
 		assertEquals(
-			number1, pair.first
-		);
+			number1, mPair.first);
 		assertEquals(
-			number2, pair.second
+			number2, mPair.second);
+	}
+
+	@Test
+	public void testToMutable() {
+		BytePair pair = mPair.toMutable();
+		assertEquals(
+			mPair.first, pair.getFirst());
+		assertEquals(
+			mPair.second, pair.getSecond());
+	}
+
+	@Test
+	public void testEquals_SameValues_ReturnsTrue() {
+		BytePairFixed pair = new BytePairFixed(number1, number2);
+		assertEquals(mPair, pair);
+	}
+
+	@Test
+	public void testEquals_DifferentValues_ReturnsFalse() {
+		BytePairFixed pair = new BytePairFixed(
+			number1, (byte) (1 + number2)
 		);
+		assertNotEquals(mPair, pair);
+		pair = new BytePairFixed(
+			(byte) (1 + number1), number2
+		);
+		assertNotEquals(mPair, pair);
+	}
+
+	@Test
+	void testEquals_WithMutableBytePair_ReturnsTrue() {
+		BytePair pair0 = mPair.toMutable();
+		assertEquals(mPair, pair0);
+	}
+
+	@Test
+	void testEquals_WithMutableBytePairDifferentValues_ReturnsFalse() {
+		BytePair pair = new BytePair(number1, (byte) 10);
+		assertNotEquals(mPair, pair);
+		pair = new BytePair((byte) 10, number2);
+		assertNotEquals(mPair, pair);
+	}
+
+	@Test
+	void testEquals_UnsupportedType_ReturnsFalse() {
+		String str = "";
+		assertNotEquals(mPair, str);
 	}
 
 }
--- numbers/src/test/java/mathtools/numbers/structs/BytePairTest.java
+++ numbers/src/test/java/mathtools/numbers/structs/BytePairTest.java
@@ -1,14 +1,25 @@
 package mathtools.numbers.structs;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /** Testing [BytePair] class
  * @author DK96-OS : 2022
  */
 public final class BytePairTest {
+
+	private BytePair mPair0;
+	private final byte value0 = 0;
+
+	@BeforeEach
+	void testSetup() {
+		mPair0 = new BytePair(value0, value0);
+	}
 
 	/** Assert that actual is not null, and it's values match expectations */
 	public static void assertPairEquals(
@@ -22,14 +33,67 @@ public final class BytePairTest {
 	}
 
 	@Test
-	void testSetters() {
-		final BytePair pair = new BytePair((byte) 0, (byte) 1);
-		pair.setFirst((byte) 8);
+	void testSetFirst() {
+		mPair0.setFirst((byte) 8);
 		assertEquals(
-			8, pair.getFirst());
-		pair.setSecond((byte) 12);
+			8, mPair0.getFirst());
 		assertEquals(
-			12, pair.getSecond());
+			value0, mPair0.getSecond());
+	}
+
+	@Test
+	void testSetSecond() {
+		mPair0.setSecond((byte) 12);
+		assertEquals(
+			value0, mPair0.getFirst());
+		assertEquals(
+			12, mPair0.getSecond());
+	}
+
+	@Test
+	void testToFixed() {
+		BytePairFixed result = mPair0.toFixed();
+		//Modify Pair0
+		mPair0.setFirst((byte) 20);
+		mPair0.setSecond((byte) -10);
+		assertEquals(
+			value0, result.first);
+		assertEquals(
+			value0, result.second);
+	}
+
+	@Test
+	void testEquals_SameValues_ReturnsTrue() {
+		BytePair pair0 = new BytePair(value0, value0);
+		assertTrue(mPair0.equals(pair0));
+	}
+
+	@Test
+	void testEquals_DifferentValues_ReturnsFalse() {
+		BytePair pair = new BytePair(value0, (byte) 2);
+		assertFalse(mPair0.equals(pair));
+		pair = new BytePair((byte) 2, value0);
+		assertFalse(mPair0.equals(pair));
+	}
+
+	@Test
+	void testEquals_WithFixedBytePair_ReturnsTrue() {
+		BytePairFixed pair0 = mPair0.toFixed();
+		assertTrue(mPair0.equals(pair0));
+	}
+
+	@Test
+	void testEquals_WithFixedBytePairDifferentValues_ReturnsFalse() {
+		BytePairFixed pair = new BytePairFixed(value0, (byte) 1);
+		assertFalse(mPair0.equals(pair));
+		pair = new BytePairFixed((byte) 1, value0);
+		assertFalse(mPair0.equals(pair));
+	}
+
+	@Test
+	void testEquals_UnsupportedType_ReturnsFalse() {
+		String str = "";
+		assertFalse(mPair0.equals(str));
 	}
 
 }
