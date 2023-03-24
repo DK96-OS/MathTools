00c87a3fe7472f9097cdadb7442b3e9309e8a430
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2023-03-24 15:25:25 -0400

    Tests: * Create LongRangeMutableTest

---
+++ numbers/src/test/java/mathtools/numbers/strict/LongRangeMutableTest.java
@@ -0,0 +1,85 @@
+package mathtools.numbers.strict;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/** Testing LongRangeMutable class.
+ * @author DK96-OS : 2023
+ */
+public class LongRangeMutableTest {
+
+	private final long start0 = 0;
+	private final long last0 = 20;
+
+	private LongRangeMutable mRange0;
+
+	@BeforeEach
+	void testSetup() {
+		mRange0 = new LongRangeMutable(start0, last0);
+	}
+
+	@Test
+	void testInitialCondition() {
+		assertEquals(start0, mRange0.getStartValue());
+		assertEquals(last0, mRange0.getLastValue());
+	}
+
+	@Test
+	void testReversedValues_ConstructorFixes() {
+		mRange0 = new LongRangeMutable(last0, start0);
+		assertEquals(start0, mRange0.getStartValue());
+		assertEquals(last0, mRange0.getLastValue());
+	}
+
+	@Test
+	void testSetStart_BelowLast_ReturnsTrue() {
+		long newStart = last0 - 2;
+		assertTrue(mRange0.setStart(newStart));
+		assertEquals(newStart, mRange0.getStartValue());
+		assertEquals(last0, mRange0.getLastValue());
+	}
+
+	@Test
+	void testSetStart_AboveLast_ReturnsFalse() {
+		long newStart = last0 + 2;
+		assertFalse(mRange0.setStart(newStart));
+		assertEquals(start0, mRange0.getStartValue());
+		assertEquals(last0, mRange0.getLastValue());
+	}
+
+	@Test
+	void testSetStart_EqualToLast_ReturnsTrue() {
+		assertTrue(mRange0.setStart(last0));
+		assertEquals(last0, mRange0.getStartValue());
+		assertEquals(last0, mRange0.getLastValue());
+	}
+
+	@Test
+	void testSetLast_AboveStart_ReturnsTrue() {
+		long newLast = start0 + 1;
+		assertTrue(mRange0.setLast(newLast));
+		assertEquals(start0, mRange0.getStartValue());
+		assertEquals(newLast, mRange0.getLastValue());
+	}
+
+	@Test
+	void testSetLast_BelowStart_ReturnsFalse() {
+		long newLast = start0 - 1;
+		assertFalse(mRange0.setLast(newLast));
+		assertEquals(start0, mRange0.getStartValue());
+		assertEquals(last0, mRange0.getLastValue());
+	}
+
+	@Test
+	void testSetLast_EqualToStart_ReturnsTrue() {
+		assertFalse(mRange0.setLast(start0));
+		assertEquals(start0, mRange0.getStartValue());
+		assertEquals(start0, mRange0.getLastValue());
+	}
+
+
+}
